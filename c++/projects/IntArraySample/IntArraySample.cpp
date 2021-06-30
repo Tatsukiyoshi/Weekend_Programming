085ae5fb04314dffab57d74a7de9c2d59180cac3
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2021-06-30 23:30:44 +0900

    Update IntArray to class

--- c++/projects/IntArraySample/IntArraySample.cpp
+++ c++/projects/IntArraySample/IntArraySample.cpp
@@ -3,62 +3,64 @@
 
 #include <iostream>
 
-// 単純な構造体でIntArrayを構築
-struct IntArray {
+// メンバ関数を持つクラスと同じオブジェクトを定義
+class IntArray {
+    // IntArrayのインタフェース関数
+public:
+    // IntArrayのインスタンスを生成、消滅
+    void init();
+    void cleanup();
+
+    // IntArrayの要素の数にアクセス
+    void setSize(size_t value);
+    size_t getSize();
+
+    // IntArrayの各要素にアクセス
+    void setElem(size_t index, int value);
+    int getElem(size_t index);
+
+private:
     int* elems;         // 配列の要素
     size_t numElems;    // 配列の要素の数
 };
-
-// IntArrayのインタフェース関数
-// IntArrayのインスタンスを生成、消滅
-void IA_init(IntArray* object);
-void IA_cleanup(IntArray* object);
-
-// IntArrayの要素の数にアクセス
-void IA_setSize(IntArray* object, size_t value);
-size_t IA_getSize(IntArray* object);
-
-// IntArrayの各要素にアクセス
-void IA_setElem(IntArray* object, size_t index, int value);
-int IA_getElem(IntArray* object, size_t index);
 
 void error(char* mes) {
     std::cerr << mes;
 }
 
-// IA_init()の定義
-void IA_init(IntArray* object) {
-    object->numElems = 0;
-    object->elems = 0;
+// init()の定義
+void IntArray::init() {
+    numElems = 0;
+    elems = 0;
 }
 
-// IA_setSize()の定義
-void IA_setSize(IntArray* object, size_t value) {
-    if (object->elems != 0) free(object->elems);
-    object->numElems = value;
-    object->elems = (int*)malloc(value * sizeof(int));
+// setSize()の定義
+void IntArray::setSize(size_t value) {
+    if (this->elems != 0) free(this->elems);
+    this->numElems = value;
+    this->elems = (int*)malloc(value * sizeof(int));
 }
 
-// IA_cleanup()の定義
-void IA_cleanup(IntArray* object) {
-    IA_setSize(object, 0);
+// cleanup()の定義
+void IntArray::cleanup() {
+    this->setSize(0);
 }
 
-// IA_getSize()の定義
-size_t IA_getSize(IntArray* object) {
-    return object->numElems;
+// getSize()の定義
+size_t IntArray::getSize() {
+    return this->numElems;
 }
 
-// IA_getElem()の定義
-int IA_getElem(IntArray* object, size_t index) {
-    if (index >= object->numElems) error((char *)"bad index");
-    return object->elems[index];
+// getElem()の定義
+int IntArray::getElem(size_t index) {
+    if (index >= this->numElems) error((char *)"bad index");
+    return this->elems[index];
 }
 
-// IA_setElem()の定義
-void IA_setElem(IntArray* object, size_t index, int value) {
-    if (index >= object->numElems) error((char *)"bad index");
-    object->elems[index] = value;
+// setElem()の定義
+void IntArray::setElem(size_t index, int value) {
+    if (index >= this->numElems) error((char *)"bad index");
+    this->elems[index] = value;
 }
 
 int main()
@@ -67,38 +69,38 @@ int main()
 
     //IntArray powersOf2 = { 0, 0 };  // 2の累乗を保持
     IntArray powersOf2;
-    IA_init(&powersOf2);
+    powersOf2.init();
     
     //powersOf2.numElems = 8;
     //powersOf2.elems = (int*)malloc(powersOf2.numElems * sizeof(int));
-    IA_setSize(&powersOf2, 8);
+    powersOf2.setSize(8);
 
     //powersOf2.elems[0] = 1;
-    IA_setElem(&powersOf2, 0, 1);
+    powersOf2.setElem(0, 1);
 
     //powersOf2.elems[1] = 2 * powersOf2.elems[0];
-    IA_setElem(&powersOf2, 1, 2 * IA_getElem(&powersOf2, 0));
+    powersOf2.setElem(1, 2 * powersOf2.getElem(0));
 
     //powersOf2.elems[2] = 2 * powersOf2.elems[1];
-    IA_setElem(&powersOf2, 2, 2 * IA_getElem(&powersOf2, 1));
+    powersOf2.setElem(2, 2 * powersOf2.getElem(1));
 
     //powersOf2.elems[3] = 2 * powersOf2.elems[2];
-    IA_setElem(&powersOf2, 3, 2 * IA_getElem(&powersOf2, 2));
+    powersOf2.setElem(3, 2 * powersOf2.getElem(2));
 
     //powersOf2.elems[4] = 2 * powersOf2.elems[3];
-    IA_setElem(&powersOf2, 4, 2 * IA_getElem(&powersOf2, 3));
+    powersOf2.setElem(4, 2 * powersOf2.getElem(3)); 
 
     //powersOf2.elems[5] = 2 * powersOf2.elems[4];
-    IA_setElem(&powersOf2, 5, 2 * IA_getElem(&powersOf2, 4));
+    powersOf2.setElem(5, 2 * powersOf2.getElem(4));
 
     //powersOf2.elems[6] = 2 * powersOf2.elems[5];
-    IA_setElem(&powersOf2, 6, 2 * IA_getElem(&powersOf2, 5));
+    powersOf2.setElem(6, 2 * powersOf2.getElem(5));
 
     //powersOf2.elems[7] = 2 * powersOf2.elems[6];
-    IA_setElem(&powersOf2, 7, 2 * IA_getElem(&powersOf2, 6));
+    powersOf2.setElem(7, 2 * powersOf2.getElem(6));
 
     //free(powersOf2.elems);
-    IA_cleanup(&powersOf2);
+    powersOf2.cleanup();
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
