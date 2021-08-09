d863c232e078b115cd12d05bbb505342205daeb9
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2021-08-09 17:21:29 +0900

    Update Member Capitalize

--- net/C#/WindowsFormsApp2/SettingForm.Designer.cs
+++ net/C#/WindowsFormsApp2/SettingForm.Designer.cs
@@ -30,12 +30,16 @@ namespace WindowsFormsApp2
         private void InitializeComponent()
         {
             this.FullColorOn = new System.Windows.Forms.RadioButton();
-            this.FullColor = new System.Windows.Forms.GroupBox();
+            this.ModeSelection = new System.Windows.Forms.GroupBox();
             this.FullColorOff = new System.Windows.Forms.RadioButton();
             this.SampleText = new System.Windows.Forms.Label();
             this.ColorSelectButton = new System.Windows.Forms.Button();
             this.CloseButton = new System.Windows.Forms.Button();
-            this.FullColor.SuspendLayout();
+            this.ScopeSelection = new System.Windows.Forms.GroupBox();
+            this.SolidOnly = new System.Windows.Forms.RadioButton();
+            this.SelectAll = new System.Windows.Forms.RadioButton();
+            this.ModeSelection.SuspendLayout();
+            this.ScopeSelection.SuspendLayout();
             this.SuspendLayout();
             // 
             // FullColorOn
@@ -43,32 +47,32 @@ namespace WindowsFormsApp2
             this.FullColorOn.AutoSize = true;
             this.FullColorOn.Location = new System.Drawing.Point(6, 18);
             this.FullColorOn.Name = "FullColorOn";
-            this.FullColorOn.Size = new System.Drawing.Size(41, 16);
-            this.FullColorOn.TabIndex = 0;
+            this.FullColorOn.Size = new System.Drawing.Size(71, 16);
+            this.FullColorOn.TabIndex = 3;
             this.FullColorOn.TabStop = true;
-            this.FullColorOn.Text = "あり";
+            this.FullColorOn.Text = "作成可能";
             this.FullColorOn.UseVisualStyleBackColor = true;
             // 
-            // FullColor
+            // ModeSelection
             // 
-            this.FullColor.Controls.Add(this.FullColorOff);
-            this.FullColor.Controls.Add(this.FullColorOn);
-            this.FullColor.Location = new System.Drawing.Point(21, 46);
-            this.FullColor.Name = "FullColor";
-            this.FullColor.Size = new System.Drawing.Size(117, 71);
-            this.FullColor.TabIndex = 1;
-            this.FullColor.TabStop = false;
-            this.FullColor.Text = "色の作成";
+            this.ModeSelection.Controls.Add(this.FullColorOff);
+            this.ModeSelection.Controls.Add(this.FullColorOn);
+            this.ModeSelection.Location = new System.Drawing.Point(12, 37);
+            this.ModeSelection.Name = "ModeSelection";
+            this.ModeSelection.Size = new System.Drawing.Size(117, 71);
+            this.ModeSelection.TabIndex = 2;
+            this.ModeSelection.TabStop = false;
+            this.ModeSelection.Text = "選択モード";
             // 
             // FullColorOff
             // 
             this.FullColorOff.AutoSize = true;
             this.FullColorOff.Location = new System.Drawing.Point(7, 41);
             this.FullColorOff.Name = "FullColorOff";
-            this.FullColorOff.Size = new System.Drawing.Size(42, 16);
-            this.FullColorOff.TabIndex = 1;
+            this.FullColorOff.Size = new System.Drawing.Size(71, 16);
+            this.FullColorOff.TabIndex = 4;
             this.FullColorOff.TabStop = true;
-            this.FullColorOff.Text = "なし";
+            this.FullColorOff.Text = "作成不可";
             this.FullColorOff.UseVisualStyleBackColor = true;
             // 
             // SampleText
@@ -77,7 +81,7 @@ namespace WindowsFormsApp2
             this.SampleText.Location = new System.Drawing.Point(19, 13);
             this.SampleText.Name = "SampleText";
             this.SampleText.Size = new System.Drawing.Size(43, 12);
-            this.SampleText.TabIndex = 2;
+            this.SampleText.TabIndex = 0;
             this.SampleText.Text = "サンプル";
             // 
             // ColorSelectButton
@@ -85,36 +89,72 @@ namespace WindowsFormsApp2
             this.ColorSelectButton.Location = new System.Drawing.Point(78, 8);
             this.ColorSelectButton.Name = "ColorSelectButton";
             this.ColorSelectButton.Size = new System.Drawing.Size(75, 23);
-            this.ColorSelectButton.TabIndex = 3;
+            this.ColorSelectButton.TabIndex = 1;
             this.ColorSelectButton.Text = "選択";
             this.ColorSelectButton.UseVisualStyleBackColor = true;
-            this.ColorSelectButton.Click += new System.EventHandler(this.button1_Click);
+            this.ColorSelectButton.Click += new System.EventHandler(this.ColorSelectButton_Click);
             // 
             // CloseButton
             // 
-            this.CloseButton.Location = new System.Drawing.Point(63, 125);
+            this.CloseButton.Location = new System.Drawing.Point(104, 125);
             this.CloseButton.Name = "CloseButton";
             this.CloseButton.Size = new System.Drawing.Size(75, 23);
-            this.CloseButton.TabIndex = 4;
+            this.CloseButton.TabIndex = 8;
             this.CloseButton.Text = "OK";
             this.CloseButton.UseVisualStyleBackColor = true;
             this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // ScopeSelection
+            // 
+            this.ScopeSelection.Controls.Add(this.SolidOnly);
+            this.ScopeSelection.Controls.Add(this.SelectAll);
+            this.ScopeSelection.Location = new System.Drawing.Point(150, 37);
+            this.ScopeSelection.Name = "ScopeSelection";
+            this.ScopeSelection.Size = new System.Drawing.Size(117, 71);
+            this.ScopeSelection.TabIndex = 5;
+            this.ScopeSelection.TabStop = false;
+            this.ScopeSelection.Text = "選択範囲";
+            // 
+            // SolidOnly
+            // 
+            this.SolidOnly.AutoSize = true;
+            this.SolidOnly.Location = new System.Drawing.Point(7, 41);
+            this.SolidOnly.Name = "SolidOnly";
+            this.SolidOnly.Size = new System.Drawing.Size(68, 16);
+            this.SolidOnly.TabIndex = 7;
+            this.SolidOnly.TabStop = true;
+            this.SolidOnly.Text = "純色のみ";
+            this.SolidOnly.UseVisualStyleBackColor = true;
+            // 
+            // SelectAll
+            // 
+            this.SelectAll.AutoSize = true;
+            this.SelectAll.Location = new System.Drawing.Point(6, 18);
+            this.SelectAll.Name = "SelectAll";
+            this.SelectAll.Size = new System.Drawing.Size(74, 16);
+            this.SelectAll.TabIndex = 6;
+            this.SelectAll.TabStop = true;
+            this.SelectAll.Text = "すべての色";
+            this.SelectAll.UseVisualStyleBackColor = true;
             // 
             // SettingForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(191, 160);
+            this.ClientSize = new System.Drawing.Size(279, 160);
             this.ControlBox = false;
             this.Controls.Add(this.CloseButton);
             this.Controls.Add(this.ColorSelectButton);
             this.Controls.Add(this.SampleText);
-            this.Controls.Add(this.FullColor);
+            this.Controls.Add(this.ScopeSelection);
+            this.Controls.Add(this.ModeSelection);
             this.Name = "SettingForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "表示色選択";
-            this.FullColor.ResumeLayout(false);
-            this.FullColor.PerformLayout();
+            this.ModeSelection.ResumeLayout(false);
+            this.ModeSelection.PerformLayout();
+            this.ScopeSelection.ResumeLayout(false);
+            this.ScopeSelection.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -123,10 +163,13 @@ namespace WindowsFormsApp2
         #endregion
 
         private System.Windows.Forms.RadioButton FullColorOn;
-        private System.Windows.Forms.GroupBox FullColor;
+        private System.Windows.Forms.GroupBox ModeSelection;
         private System.Windows.Forms.RadioButton FullColorOff;
         private System.Windows.Forms.Label SampleText;
         private System.Windows.Forms.Button ColorSelectButton;
         private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.GroupBox ScopeSelection;
+        private System.Windows.Forms.RadioButton SolidOnly;
+        private System.Windows.Forms.RadioButton SelectAll;
     }
 }
--- net/C#/WindowsFormsApp2/SettingForm.cs
+++ net/C#/WindowsFormsApp2/SettingForm.cs
@@ -19,10 +19,21 @@ namespace WindowsFormsApp2
             // サンプルテキストは、背景を黒、前景を白で表示
             SampleText.ForeColor = Color.White;
             SampleText.BackColor = Color.Black;
+
+            // 選択肢を初期設定
+            FullColorOn.Checked = true; // 作成可能
+            SolidOnly.Checked = true;   // 純色のみ
+        }
+
+        // 色選択ダイアログのクローズ
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            Program.PrintInfoLog("Clicked Close Button on SettingForm");
+            this.Hide();
         }
 
         // 色選択開始
-        private void button1_Click(object sender, EventArgs e)
+        private void ColorSelectButton_Click(object sender, EventArgs e)
         {
             // 色選択用ダイアログ
             ColorDialog ColorDialog1 = new ColorDialog();
@@ -30,7 +41,12 @@ namespace WindowsFormsApp2
             // 色選択の初期値は、サンプルテキストの表示色
             ColorDialog1.Color = SampleText.ForeColor;
 
-            // 色選択で固定値を配置する（白、赤、黄色、水色）
+            // 色選択で固定値を配置する（白、黒、赤、黄色、水色）
+            ColorDialog1.CustomColors = new int[] {Color.White.ToArgb(), Color.Black.ToArgb(),
+                Color.Red.ToArgb(), Color.Yellow.ToArgb(), Color.LightBlue.ToArgb()};
+
+            // 色選択範囲
+            ColorDialog1.SolidColorOnly = this.SolidOnly.Checked;
 
             // 色の作成可否の設定
             ColorDialog1.AllowFullOpen = this.FullColorOn.Checked;
@@ -41,16 +57,9 @@ namespace WindowsFormsApp2
             {
                 Color SelectedColor = ColorDialog1.Color;
                 SampleText.ForeColor = SelectedColor;
-                Program.PrintInfoLog("Selected:" + SelectedColor.Name + "(" + 
-                    SelectedColor.R + "," + SelectedColor.G + "," + SelectedColor.B + ")");
+                Program.PrintInfoLog("Selected:" + SelectedColor.Name + "(" +
+                        SelectedColor.R + "," + SelectedColor.G + "," + SelectedColor.B + ")");
             }
-        }
-
-        // 色選択ダイアログのクローズ
-        private void CloseButton_Click(object sender, EventArgs e)
-        {
-            Program.PrintInfoLog("Clicked Close Button on SettingForm");
-            this.Hide();
         }
     }
 }
