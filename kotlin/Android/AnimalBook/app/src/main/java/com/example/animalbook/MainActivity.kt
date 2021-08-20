35206e06543aa34cf51fc8c2c9229b9f34a1bf9a
taishow2019 <taishow2007@live.jp>
2021-08-20 17:50:20 +0900

    Update AnimalBook Project

--- kotlin/Android/AnimalBook/app/src/main/java/com/example/animalbook/MainActivity.kt
+++ kotlin/Android/AnimalBook/app/src/main/java/com/example/animalbook/MainActivity.kt
@@ -1,25 +1,20 @@
 package com.example.animalbook
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(R.layout.activity_main)
 
-        val fragment = binding.titleFragment as? TitleFragment
+        val fragment = titleFragment as? TitleFragment
         fragment?.setTitle("フラグメント動物図鑑")
 
-        binding.nextButton.setOnClickListener {
+        nextButton.setOnClickListener {
             val intent = Intent(this, SubActivity::class.java)
             startActivity(intent)
         }
--- kotlin/Android/AnimalBook/app/src/main/java/com/example/animalbook/SubActivity.kt
+++ kotlin/Android/AnimalBook/app/src/main/java/com/example/animalbook/SubActivity.kt
@@ -2,47 +2,42 @@ package com.example.animalbook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//import kotlinx.android.synthetic.main.activity_sub.*
+import kotlinx.android.synthetic.main.activity_sub.*
 
 class SubActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySubBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_sub)
-        binding = ActivitySubBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(R.layout.activity_sub)
 
-        binding.lionButton.setOnClickListener {
+        lionButton.setOnClickListener {
             val fragment = LionFragment()
-            val fragmentManager = this.getSupportFragmentManager()
+            val fragmentManager = this.supportFragmentManager
             val fragmentTransaction = fragmentManager.beginTransaction()
             fragmentTransaction.replace(R.id.container, fragment)
                 .addToBackStack(null)
                 .commit()
         }
 
-        binding.hippoButton.setOnClickListener {
+        hippoButton.setOnClickListener {
             val fragment = HippoFragment()
-            val fragmentManager = this.getSupportFragmentManager()
+            val fragmentManager = this.supportFragmentManager
             val fragmentTransaction = fragmentManager.beginTransaction()
             fragmentTransaction.replace(R.id.container, fragment)
                 .addToBackStack(null)
                 .commit()
         }
 
-        binding.giraffeButton.setOnClickListener {
+        giraffeButton.setOnClickListener {
             val fragment = GiraffeFragment()
-            val fragmentManager = this.getSupportFragmentManager()
+            val fragmentManager = this.supportFragmentManager
             val fragmentTransaction = fragmentManager.beginTransaction()
             fragmentTransaction.replace(R.id.container, fragment)
                 .addToBackStack(null)
                 .commit()
         }
 
-        val fragment = binding.titleFragment as? TitleFragment
+        val fragment = titleFragment as? TitleFragment
         fragment?.setTitle("図鑑画面")
     }
 }
