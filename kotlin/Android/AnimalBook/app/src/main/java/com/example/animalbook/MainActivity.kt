176dacd4e6faa79707f4ff872dd3125de0ff2b4f
taishow2019 <taishow2007@live.jp>
2021-08-21 17:36:56 +0900

    Update AnimalBook Project

--- kotlin/Android/AnimalBook/app/src/main/java/com/example/animalbook/MainActivity.kt
+++ kotlin/Android/AnimalBook/app/src/main/java/com/example/animalbook/MainActivity.kt
@@ -3,20 +3,36 @@ package com.example.animalbook
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.commit
+import com.example.animalbook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        val fragment = titleFragment as? TitleFragment
-        fragment?.setTitle("フラグメント動物図鑑")
+        // フラグメントの生成およびタイトル設定
+        val fragment = TitleFragment()
+        fragment.setTitle("フラグメント動物図鑑")
 
-        nextButton.setOnClickListener {
+        // フラグメントマネージャーの取得
+        val fragmentManager: FragmentManager = this.supportFragmentManager
+
+        // アクティビティにフラグメントを追加
+        fragmentManager.commit {
+            replace(R.id.container, fragment)
+            addToBackStack(null)
+        }
+
+        binding.nextButton.setOnClickListener {
             val intent = Intent(this, SubActivity::class.java)
             startActivity(intent)
         }
     }
 }
+
