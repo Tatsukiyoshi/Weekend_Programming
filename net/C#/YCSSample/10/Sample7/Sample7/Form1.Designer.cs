2b9ecbb88dd5fa7779a4bf88168ca4882360bf13
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2022-01-15 11:05:53 +0900

    [C#]Completed Sample 7 of Lesson 10

---
+++ net/C#/YCSSample/10/Sample7/Sample7/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿using System.Xml;
+    
+    namespace Sample7
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        private TreeView tv;
+        private const String xmlPath = "..\\..\\..\\Sample.xml";
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "サンプル";
+
+            tv = new TreeView();
+            tv.Dock = DockStyle.Fill;
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(xmlPath);
+
+            XmlNode xmlRoot = doc.DocumentElement;
+            TreeNode treeRoot = new TreeNode();
+            treeRoot.Text = xmlRoot.Name;
+            tv.Nodes.Add(treeRoot);
+
+            walk(xmlRoot, treeRoot);
+
+            tv.Parent = this;
+        }
+
+        private void walk(XmlNode ixn, TreeNode itn)
+        {
+            for(XmlNode ch = ixn.FirstChild; ch != null; ch = ch.NextSibling)
+            {
+                TreeNode tn = new TreeNode();
+                itn.Nodes.Add(tn);
+                walk(ch, tn);
+
+                switch (ch.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        tn.Text = ch.Name;
+                        break;
+
+                    default:
+                        tn.Text = ch.Value;
+                        break;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ net/C#/YCSSample/10/Sample7/Sample7/Form1.cs
@@ -0,0 +1,12 @@
+using System.Xml;
+
+namespace Sample7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
