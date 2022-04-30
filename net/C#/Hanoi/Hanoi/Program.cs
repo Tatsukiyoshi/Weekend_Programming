491a62424112b0f41f85051fe4f3e2191dcd8a39
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2022-04-30 14:39:15 +0900

    [C#]Hanoi Solution

---
+++ net/C#/Hanoi/Hanoi/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Hanoi
+{
+    public static int count;
+
+    public static void Main()
+    {
+        count = 0;
+
+        HanoiR(6, "Left", "Center", "Right");
+    }
+
+    public static void HanoiR(int n, string from, string to, string work)
+    {
+        if(n > 0)
+        {
+            HanoiR(n - 1, from, work, to);
+            Console.WriteLine(++count + ")" + n + ":" + from + "->" + to);
+            HanoiR(n - 1, work, to, from);
+        }
+    }
+}
