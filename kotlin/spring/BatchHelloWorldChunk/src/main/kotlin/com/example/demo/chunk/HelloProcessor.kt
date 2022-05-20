83c90c294c4bdbf1980bc22bdc2165554b8e5cd0
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2022-05-21 01:43:10 +0900

    [Spring Batch]Chapter 5 finished by Kotlin

--- kotlin/spring/BatchHelloWorldChunk/src/main/kotlin/com/example/demo/chunk/HelloProcessor.kt
+++ kotlin/spring/BatchHelloWorldChunk/src/main/kotlin/com/example/demo/chunk/HelloProcessor.kt
@@ -2,8 +2,11 @@ package com.example.demo.chunk
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.batch.core.StepExecution
+import org.springframework.batch.core.annotation.BeforeStep
 import org.springframework.batch.core.configuration.annotation.StepScope
 import org.springframework.batch.item.ItemProcessor
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 @Component
@@ -12,8 +15,19 @@ class HelloProcessor: ItemProcessor<String, String> {
     companion object {
         private var log: Logger = LoggerFactory.getLogger(this::class.java)
     }
-
     private var _item: String? = null
+
+    @Value("#{JobExecutionContext['jobKey']}")
+    private var jobValue: String? = null
+
+    @Value("#{StepExecutionContext['stepKey']}")
+    private var stepValue: String? = null
+
+    @BeforeStep
+    fun beforeStep(stepExecution: StepExecution){
+        log.info("jobKey={}", jobValue)
+        log.info("stepKey={}", stepValue)
+    }
 
     override fun process(item: String): String? {
         // 文字列の加工
