307b3812934e2c87604275a4434e6953a6046778
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2022-06-05 17:34:49 +0900

    [Spring Batch]Chapter 8 finished (#3)

--- kotlin/spring/batchcsvimport/src/main/kotlin/com/example/demo/domain/model/Employee.kt
+++ kotlin/spring/batchcsvimport/src/main/kotlin/com/example/demo/domain/model/Employee.kt
@@ -1,18 +1,23 @@
 package com.example.demo.domain.model
 
 import lombok.Data
+import javax.persistence.Entity
+import javax.persistence.Id
 import javax.validation.constraints.Min
 import javax.validation.constraints.NotNull
 
 @Data
+@Entity
 open class Employee {
     @NotNull
+    @Id
     var id: Int? = 0
     @NotNull
     var name: String? = null
     @Min(20)
     var age: Int? = 0
     var gender: Int? = 0
+    @Transient
     var genderString: String? = null
 
     /** 性別の文字列を数値に変換 */
