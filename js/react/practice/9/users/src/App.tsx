83aac42dd632ac8687db613d9f12c23b0da07895
Tatsukiyoshi <taishow2007@live.jp>
2022-07-02 12:08:44 +0900

    [React]Chapter 9 finished (#12)

--- js/react/practice/9/users/src/App.tsx
+++ js/react/practice/9/users/src/App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import { useFetchUsers } from "./hooks/useFetchUsers";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // カスタムフックの使用
+  // 関数を実行し返却値を分割代入で受け取る
+  const { userList, isLoading, isError, onClickFetchUser } = useFetchUsers();
 }
 
 export default App;
