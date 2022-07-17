61c0a4fabc68f10bef5b8dde4fdcbe564f5be0ae
Tatsukiyoshi <taishow2007@live.jp>
2022-07-17 23:03:18 +0900

    [Java]Organize Sample Codes (#14)

---
+++ java/eclipse-workspace/Chapter 04/StudntComparator/src/Student/Student.java
@@ -0,0 +1,49 @@
+package Student;
+
+/**
+ * 生徒の点数を扱うクラス
+ */
+public class Student {
+	/** 名前 */
+	String name;
+	
+	/** 点数 */
+	int score;
+	
+	/** 最高点 */
+	static final int MAX_SCORE = 100;
+	
+	/** 名前と点数を指定してインスタンスを生成します
+	 * @param name　名前
+	 * @param score 点数
+	 */
+	Student(String name, int score){
+		this.name = name;
+		this.score = score;
+	}
+	
+	/** 名前を指定してインスタンスを生成します
+	 * @param name 名前
+	 */
+	Student(String name){
+		this(name, 0);
+	}
+
+	/** 点数を標準出力に表示します
+	 */
+	void printScore() {
+		System.out.println(name + "さんは" + MAX_SCORE + "点満点中、" + score + "点です。");
+	}
+	
+	/** 名前を取得します
+	 */
+	public String getName() {
+		return this.name;
+	}
+	
+	/** 点数を取得します
+	 */
+	public int getScore() {
+		return this.score;
+	}
+}
