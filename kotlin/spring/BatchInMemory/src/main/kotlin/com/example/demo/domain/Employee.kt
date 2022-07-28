8930148dad4c4a49282c22505a56447b50d62f75
Tatsukiyoshi <taishow2007@live.jp>
2022-07-29 08:48:00 +0900

    インメモリDB実装見直し

--- kotlin/spring/BatchInMemory/src/main/kotlin/com/example/demo/domain/Employee.kt
+++ kotlin/spring/BatchInMemory/src/main/kotlin/com/example/demo/domain/Employee.kt
@@ -3,23 +3,4 @@ package com.example.demo.domain
 import lombok.Data
 
 @Data
-open class Employee{
-    private var id: Int
-    private var name: String
-    private var age: Int
-    private var gender: Int
-
-    protected constructor() {
-        this.id = 0
-        this.name = ""
-        this.age = 0
-        this.gender = 0
-    }
-
-    constructor(id: Int, name: String, age: Int, gender: Int){
-        this.id = id
-        this.name = name
-        this.age = age
-        this.gender = gender
-    }
-}
+open class Employee(open var id: Int, open var name: String, open var age: Int, open var gender: Int)
