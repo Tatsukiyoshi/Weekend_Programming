c92353b144541dd1fa95a31f0bcbb3cbb5a09cbd
Tatsukiyoshi <taishow2007@live.jp>
2022-08-07 22:53:01 +0900

    [Spring Batch]Chapter 14.3 try now (#16)

---
+++ kotlin/spring/BatchCsvExport/src/test/kotlin/com/example/demo/config/TestConfig.kt
@@ -0,0 +1,21 @@
+package com.example.demo.config
+
+import org.springframework.batch.core.Job
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+
+@Configuration
+class TestConfig {
+    @Autowired
+    @Qualifier("JdbcCursorJob")
+    private lateinit var jdbcCursorJob: Job
+
+    @Bean
+    @Primary
+    fun testJob(): Job {
+        return jdbcCursorJob
+    }
+}
