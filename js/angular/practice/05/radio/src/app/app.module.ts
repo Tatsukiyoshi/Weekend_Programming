2e2403256b7e9327e5746aa9f80eb0af4a049d7d
Tatsukiyoshi <taishow2007@live.jp>
2022-08-21 02:03:37 +0900

    [Angular]Chapter 5.2.2 Arranged (#76)

--- js/angular/practice/05/radio/src/app/app.module.ts
+++ js/angular/practice/05/radio/src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatRadioModule } from '@angular/material/radio';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, FormsModule, BrowserAnimationsModule, MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
