208c3d2c4294dddc1b7f5501dd682424cda8fca2
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2022-09-18 14:30:46 +0900

    [Java]Servlet & JSP Chapter 10.6 Finished (#15)

--- java/web-workspace/docoTsubu/src/main/java/servlet/Main.java
+++ java/web-workspace/docoTsubu/src/main/java/servlet/Main.java
@@ -13,6 +13,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import model.Mutter;
+import model.PostMutterLogic;
 import model.User;
 
 /**
@@ -65,7 +66,33 @@ public class Main extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		// リクエストパラメータの取得
+		request.setCharacterEncoding("UTF-8");
+		String text = request.getParameter("text");
+		
+		// 入力値チェック
+		if(text != null && text.length() != 0) {
+			// アプリケーションスコープに保存されたつぶやきリストを取得
+			ServletContext application = this.getServletContext();
+			List<Mutter> mutterList =
+					(List<Mutter>)application.getAttribute("mutterList");
+			
+			// セッションスコープに保存されたユーザ情報を取得
+			HttpSession session = request.getSession();
+			User loginUser = (User)session.getAttribute("loginUser");
+			
+			// つぶやきをつぶやきリストに追加
+			Mutter mutter = new Mutter(loginUser.getName(), text);
+			PostMutterLogic postMutterLogic = new PostMutterLogic();
+			postMutterLogic.execute(mutter, mutterList);
+			
+			// アプリケーションスコープにつぶやきリストを保存
+			application.setAttribute("mutterList", mutterList);
+			
+			// メイン画面にフォワード
+			RequestDispatcher dispatcher = 
+					request.getRequestDispatcher("/WEB-INF/jsp/main.jsp");
+			dispatcher.forward(request, response);
+		}
 	}
-
 }
