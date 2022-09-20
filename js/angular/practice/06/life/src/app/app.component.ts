9ef8b70327efeaef9c8c3565f4e016f83b75d7da
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2022-09-20 20:59:13 +0900

    [Angular]Chapter 6.2.1/6.2.2 Finished (#13)

---
+++ js/angular/practice/06/life/src/app/app.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnChanges, OnInit, DoCheck,
+  AfterContentInit, AfterContentChecked,
+  AfterViewInit, AfterViewChecked, OnDestroy, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnChanges,
+  OnInit, DoCheck, AfterContentInit, AfterContentChecked,
+  AfterViewInit, AfterViewChecked, OnDestroy
+{
+  title = 'life';
+  show = true;
+  current = new Date();
+
+  // チェックボックス変更時に実行
+  onchange(){
+    this.show = !this.show;
+    this.current = new Date();
+  }
+
+  constructor(){
+    console.log('constructor');
+  }
+
+  // ライフサイクルメソッド（それぞれのタイミングでログを表示）
+  ngOnInit(): void {
+      console.log('ngOnInit');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges');
+  }
+      
+  ngDoCheck(): void {
+    console.log('ngDoCheck');      
+  }
+
+  ngAfterContentInit(): void {
+      console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked(): void {
+      console.log('ngAfterContentInit');
+  }
+
+  ngAfterViewInit(): void {
+      console.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked(): void {
+      console.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy(): void {
+      console.log('ngOnDestroy');
+  }
+}
