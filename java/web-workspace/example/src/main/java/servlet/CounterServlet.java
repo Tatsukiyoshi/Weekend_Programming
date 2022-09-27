6e8f6c4c6ac11a51c7cf3890d224f90bc3a7ee93
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2022-09-27 22:16:08 +0900

    [Java]Servlet & JSP Chapter 11.1.3 Finished (#15)

---
+++ java/web-workspace/example/src/main/java/servlet/CounterServlet.java
@@ -0,0 +1,82 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CounterServlet
+ */
+@WebServlet("/CounterServlet")
+public class CounterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CounterServlet() {
+        super();
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+
+		// 訪問回数を表すIntegerインスタンスを新規作成し、
+		// アプリケーションスコープに保存
+		Integer count = 0;
+		ServletContext application = config.getServletContext();
+		application.setAttribute("count", count);
+		
+		System.out.println("init() is executed");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		System.out.println("destroy() is executed");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// アプリケーションスコープに保存された訪問回数を増加
+		ServletContext application = this.getServletContext();
+		Integer count = (Integer)application.getAttribute("count");
+		count++;
+		application.setAttribute("count", count);
+		
+		// HTMLを出力
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>訪問回数を表示</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<p>訪問回数：" + count + "</p>");
+		out.println("<a href=\"/example/CounterServlet\">更新</a>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
