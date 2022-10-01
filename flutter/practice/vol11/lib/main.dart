44d4b7c8ae2ad3ed5c32a4ef7919b932e0cb06d1
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2022-10-01 21:39:05 +0900

    trial: flutter practice vol11 (#111) (#153)
    * [flutter] Web Tutorial Vol 11 Started (#111)
    
    * [flutter] Web Tutorial Vol 11 Finished (#111)

---
+++ flutter/practice/vol11/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:vol11/AlignWidget.dart';
+import 'package:vol11/AspectRatioWidget.dart';
+import 'package:vol11/CenterWidget.dart';
+import 'package:vol11/ColumnWidget.dart';
+import 'package:vol11/ContainerWidget.dart';
+import 'package:vol11/PaddingWidget.dart';
+import 'package:vol11/RowWidget.dart';
+import 'package:vol11/WrapWidget.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Layouts',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Layouts'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      /*
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+
+      */
+      body: Center(
+        child : Container(
+        alignment: Alignment.center,
+          width: double.infinity,
+          height: double.infinity,
+          color: Colors.grey,
+          child: WrapWidget(),
+        )
+      ),
+    );
+  }
+}
