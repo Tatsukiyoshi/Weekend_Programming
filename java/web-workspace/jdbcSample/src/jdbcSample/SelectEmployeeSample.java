ed17cfd7dde26603dd0a24b4444a00d26596344b
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2022-10-03 01:20:56 +0900

    [Java]Servlet & JSP Chapter 13 Finished (#15)

--- java/web-workspace/jdbcSample/src/jdbcSample/SelectEmployeeSample.java
+++ java/web-workspace/jdbcSample/src/jdbcSample/SelectEmployeeSample.java
@@ -16,5 +16,26 @@ public class SelectEmployeeSample {
 			System.out.println("名前:" + employee.getName());
 			System.out.println("年齢:" + employee.getAge() + "\n");
 		}
+		
+		if(empDAO.remove("EMP999")) {
+			System.out.println("削除に成功しました");
+		} else {
+			System.out.println("削除に失敗しました");
+		}
+
+		if(empDAO.remove("EMP001")) {
+			System.out.println("削除に成功しました");
+		} else {
+			System.out.println("削除に失敗しました");
+		}
+
+		empList = empDAO.findAll();
+
+		// 結果表に格納されたレコードの内容を表示
+		for(Employee employee : empList){
+			System.out.println("ID:" + employee.getId());
+			System.out.println("名前:" + employee.getName());
+			System.out.println("年齢:" + employee.getAge() + "\n");
+		}
 	}
 }
