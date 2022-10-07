1301f1f36f277bbc676b8b1978b72b31a15c946f
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2022-10-07 23:21:48 +0900

    trial: flutter practice vol12 (#158)
    * [flutter] Update 3.3.4
    
    * [flutter] Practice vol.12 Started (#111)
    
    * [flutter] Practice vol.12 Finished (#111)

---
+++ flutter/practice/vol12/android/app/src/main/kotlin/com/example/vol12/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.vol12
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter/practice/vol12/lib/list/MainContent.dart
@@ -0,0 +1,84 @@
+//import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+@immutable
+class MainContent extends StatelessWidget{
+
+  final Function onPressed;
+  const MainContent(this.onPressed, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(
+          height: 20,
+          child : Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+                padding: EdgeInsets.all(2),
+                child: FittedBox(
+                  fit: BoxFit.contain,
+                  child: Text("タイトル",
+                    style: TextStyle(
+                        color: Colors.black,
+                        decoration: TextDecoration.none,
+                        fontWeight: FontWeight.normal
+                    ),
+                  ),
+                )
+            ),
+          )
+        ),
+
+        Expanded(
+          child: Container(
+          color: Colors.grey.shade300,
+          )
+        ),
+        Container(
+          height: 20,
+          padding: const EdgeInsets.symmetric( horizontal: 5, vertical: 2),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ElevatedButton(
+                  onPressed: () => { onPressed() },
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+                      foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5.0),
+                              side: const BorderSide(color: Colors.red)
+                          )
+                      )
+                  ),
+                  child: const Padding(
+                      padding: EdgeInsets.all(2),
+                      child: FittedBox(
+                        fit: BoxFit.contain,
+                        child: Text("詳細を見る"),
+                      )
+                  )
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 5),
+                child: FittedBox(
+                  fit: BoxFit.fitHeight,
+                  child: Text("2021/07/21",
+                    style: TextStyle(
+                        color: Colors.black,
+                        decoration: TextDecoration.none,
+                        fontWeight: FontWeight.normal
+                    ),
+                  ),
+                )
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
