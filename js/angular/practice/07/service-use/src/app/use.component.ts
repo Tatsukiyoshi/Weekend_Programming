dec9311154525744478cfd8a80ff9328853094e5
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2022-10-09 00:46:00 +0900

    [Angular] List 7-9 of Chapter 7.2.2 Finished (#13)

---
+++ js/angular/practice/07/service-use/src/app/use.component.ts
@@ -0,0 +1,20 @@
+import { Component } from "@angular/core";
+import { UseService } from "./use.service";
+
+@Component({
+    selector: 'my-use',
+    
+    // サービスインスタンスの生成方法を宣言
+    providers: [
+        {provide: UseService, useClass: UseService }
+    ],
+    template: `<li>UseService : {{current}}</li>`
+})
+export class UseComponent {
+    current: string;
+
+    // UseService#showメソッドで、サービスの生成時刻を表示
+    constructor(private use: UseService) {
+        this.current = use.show();
+    }
+}
