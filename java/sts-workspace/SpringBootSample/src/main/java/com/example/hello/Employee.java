89370b2aa4b4dc8aeee1120bd2d9b926e985b775
Tatsukiyoshi <48684470+Tatsukiyoshi@users.noreply.github.com>
2022-10-08 16:04:55 +0900

    [Spring Boot]Chapter 6.1.1 finished (#17)

--- java/sts-workspace/SpringBootSample/src/main/java/com/example/hello/Employee.java
+++ java/sts-workspace/SpringBootSample/src/main/java/com/example/hello/Employee.java
@@ -7,4 +7,26 @@ public class Employee {
 	private String employeeId;
 	private String employeeName;
 	private int employeeAge;
+
+	public String getEmployeeId() {
+		return employeeId;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public int getEmployeeAge() {
+		return employeeAge;
+	}
+	
+	public void setEmployeeId(String employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public void setEmployeeName(String name) {
+		this.employeeName = name;
+	}
+	
+	public void setEmployeeAge(int age) {
+		this.employeeAge = age;
+	}
 }
