89f61ecc6da6f3c476f5b6116d57b3071e90dffa
Tatsukiyoshi <48684470+Tatsukiyoshi@users.noreply.github.com>
2022-10-08 20:21:44 +0900

    [Spring Boot]Chapter 6.3.2 finished (#17)

--- java/sts-workspace/SpringBootSample/src/main/java/com/example/controller/SignupController.java
+++ java/sts-workspace/SpringBootSample/src/main/java/com/example/controller/SignupController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,7 +42,7 @@ public class SignupController {
 	/** ユーザ登録処理 */
 	@PostMapping("/signup")
 	public String postSignUp(Model model, Locale locale,
-			@ModelAttribute SignupForm form,
+			@ModelAttribute @Validated SignupForm form,
 			BindingResult bindingResult) {
 		// 入力チェック結果
 		if(bindingResult.hasErrors()) {
