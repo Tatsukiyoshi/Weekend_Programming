7d9ca57424b176bbb0f678dd170667efe23d1f25
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2022-10-09 14:28:12 +0900

    [Angular] List 7-28 Finished (#13)

--- js/angular/practice/07/service-use/src/main.ts
+++ js/angular/practice/07/service-use/src/main.ts
@@ -2,11 +2,15 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { UseService } from './app/use.service';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule, {
+  providers: [
+    { provide: UseService, useValue: new UseService() }
+  ],
+}).catch(err => console.error(err));
