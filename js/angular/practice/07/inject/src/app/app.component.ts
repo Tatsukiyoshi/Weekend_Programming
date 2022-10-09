25a8b8b4263442e3a089579f525264808c2b279a
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2022-10-09 20:12:50 +0900

    [Angular] Chapter 7.3.4 Finished (#13)

---
+++ js/angular/practice/07/inject/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, Injector } from '@angular/core';
+
+import { Book } from './book';
+import { BookService } from './book.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'inject';
+
+  books!: Book[];
+
+  // Injectorサービスを有効化
+  constructor(private injector: Injector){}
+
+  // コンポーネント初期化時に、サービス経由で書籍情報を取得
+  ngOnInit(){
+    // インジェクター経由でサービスを取得
+    let bookservice = this.injector.get(BookService);
+    this.books = bookservice.getBooks();
+  }
+}
