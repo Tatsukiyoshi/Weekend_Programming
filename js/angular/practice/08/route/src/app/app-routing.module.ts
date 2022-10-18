404d5c2bac047356d399729dbb813b778dbff492
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2022-10-18 22:38:42 +0900

    [Angular] Chapter 8.3.3 Finished (#13)

--- js/angular/practice/08/route/src/app/app-routing.module.ts
+++ js/angular/practice/08/route/src/app/app-routing.module.ts
@@ -5,12 +5,15 @@ import { ExampleComponent } from './example.component';
 import { MainComponent } from './main.component';
 import { ArticleComponent } from './article.component';
 import { ParamComponent } from './param.component';
+import { DataComponent } from './data.component';
 
 const routes: Routes = [
   { path: 'exam', component: ExampleComponent },
   { path: '', component: MainComponent},
   { path: 'article/:id', component: ArticleComponent },
   { path: 'param', component: ParamComponent },
+  { path: 'data', component: DataComponent,
+    data: { category: 'Angular'}},
   { path: '**', redirectTo: '/' },
 ];
 
