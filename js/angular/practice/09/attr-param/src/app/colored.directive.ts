07c2f921652d80cf5c71ad9f44b0bf2b6f11934d
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2022-10-21 22:36:09 +0900

    [Angular] Chapter 9.2.2 Finished (#13)

--- js/angular/practice/09/attr-param/src/app/colored.directive.ts
+++ js/angular/practice/09/attr-param/src/app/colored.directive.ts
@@ -4,13 +4,13 @@ import { Directive, ElementRef, Input, OnInit } from "@angular/core";
     selector: '[myColored]'
 })
 export class ColoredDirective implements OnInit {
-    @Input() myBgcolor = '#cff';
+    @Input('myColored') color = '#cff';
 
     constructor(private el: ElementRef){
 
     }
 
     ngOnInit(): void {
-        this.el.nativeElement.style.backgroundColor = this.myBgcolor;
+        this.el.nativeElement.style.backgroundColor = this.color;
     }
 }
