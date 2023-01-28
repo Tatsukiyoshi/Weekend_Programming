5921d15c663383706d440885c65a2a5053240f79
Tatsukiyoshi <48684470+Tatsukiyoshi@users.noreply.github.com>
2023-01-29 01:40:01 +0900

    BugFix: AnimalBook (#654)

--- kotlin/Android/AnimalBook/app/src/main/java/com/example/animalbook/MainActivity.kt
+++ kotlin/Android/AnimalBook/app/src/main/java/com/example/animalbook/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.animalbook
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.commit
 import com.example.animalbook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,23 +14,9 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        // フラグメントの生成およびタイトル設定
-        val fragment = TitleFragment()
-        // fragment.setTitle("フラグメント動物図鑑")
-
-        // フラグメントマネージャーの取得
-        val fragmentManager: FragmentManager = this.supportFragmentManager
-
-        // アクティビティにフラグメントを追加
-        fragmentManager.commit {
-//          replace(R.id.container, fragment)
-            addToBackStack(null)
-        }
-
         binding.nextButton.setOnClickListener {
             val intent = Intent(this, SubActivity::class.java)
             startActivity(intent)
         }
     }
 }
-
