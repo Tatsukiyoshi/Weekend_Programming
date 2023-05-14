a39475fe097b8c65fbfb136235f8662fc2f0478b
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2023-05-14 16:28:17 +0900

    Add: Chapter 4-8 (#772)

---
+++ rust/PerfectRust/Chapter4/str_type/src/main.rs
@@ -0,0 +1,18 @@
+mod str_type;
+
+fn main() {
+    println!("文字列リテラル");
+    str_type::declar();
+    println!("文字列の連結");
+    str_type::binding();
+    println!("文字列の変換");
+    str_type::convert();
+    println!("文字列の検査");
+    str_type::check();
+    println!("文字列の取得");
+    str_type::get();
+    println!("文字列の生成／置換");
+    str_type::repeat_replace();
+    println!("文字列の大文字／小文字変換");
+    str_type::case_converion();
+}
