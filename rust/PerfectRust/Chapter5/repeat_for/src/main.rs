5d3a5b004959941e71a217dbfc9ce0498285394f
Shinya Watanabe <taishow2007@live.jp>
2023-09-09 19:21:12 +0900

    Add: Chapter 5-3 (#772)

--- rust/PerfectRust/Chapter5/repeat_for/src/main.rs
+++ rust/PerfectRust/Chapter5/repeat_for/src/main.rs
@@ -13,6 +13,36 @@ pub fn for_1(){
     }
 }
 
+/// ### リスト5.10 カウンタのデクリメントを利用したfor式
+#[allow(dead_code)]
+pub fn for_2(){
+    println!("Loop from 4 to 0");
+    for i in (0..5).rev() {
+        print!("i = {} ", i);        
+    }
+    print!("\n");
+    println!("Loop from 5 to 0");
+    for i in (0 ..= 5).rev() {
+        print!("i = {} ", i);
+    }
+}
+
+/// ### リスト5.11 要素集合から値を取得する
+#[allow(dead_code)]
+pub fn for_3(){
+    let values = vec![100, 200, 300, 400, 500];
+    let mut result :i32 = 0;
+
+    for value in values {
+        result += value;
+    }
+    println!("result = {}", result);
+}
+
 fn main() {
     for_1();
+    println!("");
+    for_2();
+    println!("");
+    for_3();
 }
