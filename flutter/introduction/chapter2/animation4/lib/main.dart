8d41b26992bf3eae4e2add1e58a885cdadce2f00
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2023-09-18 11:29:32 +0900

    Finish: Flutter Introduction Chapter 2 (#858)
    * Add: Flutter Introduction Step1 of Chapter2 (#812)
    
    * Add: Flutter Introduction Step2 of Chapter2 (#812)
    
    * Add: Flutter Introduction Step3 of Chapter2 (#812)
    
    * Add: Flutter Introduction Step4 of Chapter2 (#812)
    
    * Add: Chapter2 / Section 01 (#812)
    
    * Add: Chapter2 / Section 02 (#812)
    
    * Add: Chapter2 / Section 02 (#812)
    
    * Add: Chapter2 / Section 03 (#812)
    
    * Add: Chapter2 / Section 05 (#812)
    
    * Add: Chapter2 / Section 06 (#812)
    
    * Add: Chapter2 / Section 07 (#812)
    
    * Fix: Chapter2 / Section 07 (#812)

---
+++ flutter/introduction/chapter2/animation4/lib/main.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a blue toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Animation 4(Tween)'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animationDouble;
+  final Tween<double> _tweenDouble = Tween(begin: 0.0, end: 200.0);
+  late Animation<Color?> _animationColor;
+  final ColorTween _tweenColor =
+      ColorTween(begin: Colors.green, end: Colors.blue);
+
+  // 再生
+  _forward() async {
+    setState(() {
+      _animationController.forward();
+    });
+  }
+
+  // 停止
+  _stop() async {
+    setState(() {
+      _animationController.stop();
+    });
+  }
+
+  // 逆再生
+  _reverse() async {
+    setState(() {
+      _animationController.reverse();
+    });
+  }
+
+  // 生成
+  @override
+  void initState() {
+    super.initState();
+    _animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 3));
+    _animationDouble = _tweenDouble.animate(_animationController);
+    _animationDouble.addListener(() {
+      setState(() {});
+    });
+    _animationColor = _tweenColor.animate(_animationController);
+    _animationColor.addListener(() {
+      setState(() {});
+    });
+  }
+
+  // 破棄
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("AnimationController:${_animationController.value}"),
+              Text("AnimationDouble:${_animationDouble.value}"),
+              Text("AnimationColor:${_animationColor.value}"),
+              SizeTransition(
+                sizeFactor: _animationController,
+                child: Center(
+                    child: SizedBox(
+                        width: _animationDouble.value,
+                        height: _animationDouble.value,
+                        child: Container(color: _animationColor.value))),
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FloatingActionButton(
+                onPressed: _forward, child: const Icon(Icons.arrow_forward)),
+            FloatingActionButton(
+                onPressed: _stop, child: const Icon(Icons.pause)),
+            FloatingActionButton(
+                onPressed: _reverse, child: const Icon(Icons.arrow_back))
+          ],
+        ));
+  }
+}
