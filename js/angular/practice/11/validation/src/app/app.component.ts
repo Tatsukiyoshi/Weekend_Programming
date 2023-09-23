29b577a356e346d49c929b55ea04c8a6d4ba02f5
Shinya Watanabe <taishow2007@live.jp>
2023-09-23 13:37:43 +0900

    Practice: Chapter 11.1.2 Solved (#13)

--- js/angular/practice/11/validation/src/app/app.component.ts
+++ js/angular/practice/11/validation/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { CustomValidators } from 'ng2-validation';
 
 @Component({
   selector: 'app-root',
@@ -6,16 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private builder: FormBuilder) {}
   title = 'validation';
-  user = {
-    url: '',
-    mail: 'hoge@example.com',
-    mail_confirm: ''
-  };
+
+  url = new FormControl('',
+    [
+      Validators.required,
+      CustomValidators.url
+    ]
+  );
+  mail = new FormControl('hoge@example.com',
+    [
+      Validators.required,
+      Validators.email
+    ]
+  );
+  mail_confirm = new FormControl('',
+    [
+      CustomValidators.equalTo(this.mail)
+    ]
+  );
+
+  myForm = this.builder.group({
+    url: this.url,
+    mail: this.mail,
+    mail_confirm: this.mail_confirm
+  });
 
   show(){
-    console.log('URL： ' + this.user.url);
-    console.log('メールアドレス： ' + this.user.mail);
-    console.log('メールアドレス（確認）： ' + this.user.mail_confirm);
+    console.log('URL： ' + this.url);
+    console.log('メールアドレス： ' + this.mail);
+    console.log('メールアドレス（確認）： ' + this.mail_confirm);
   }
 }
--- js/angular/practice/11/validation/src/app/app.module.ts
+++ js/angular/practice/11/validation/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { CustomFormsModule } from 'ng2-validation';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +9,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule, FormsModule, CustomFormsModule
+    BrowserModule, ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
