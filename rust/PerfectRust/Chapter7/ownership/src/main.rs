452ed3566f90fd9f9d0f57524dfc1a702a785a54
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2023-10-09 21:44:38 +0900

    Add: Chapter 7-4 (#772)

--- rust/PerfectRust/Chapter7/ownership/src/main.rs
+++ rust/PerfectRust/Chapter7/ownership/src/main.rs
@@ -1,9 +1,13 @@
-use crate::ownership::{ownership_2, ownership_3, ownership_4};
+use crate::ownership::{ownership_2, ownership_3, ownership_5, ownership_6};
 
 mod ownership;
 
 fn main() {
+    //ownership_1();
     ownership_2();
     ownership_3();
-    ownership_4();
+    //ownership_4();
+    ownership_5();
+    ownership_6();
+    //ownership_7();
 }
