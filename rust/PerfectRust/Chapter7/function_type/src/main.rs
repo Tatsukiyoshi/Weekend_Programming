fe7c6c49e160305e5b68ea1c3fd7528d7d00419c
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2023-10-09 20:58:29 +0900

    Add: Chapter 7-2 (#772)

---
+++ rust/PerfectRust/Chapter7/function_type/src/main.rs
@@ -0,0 +1,9 @@
+use crate::function_type::{use_function_1, use_function_2, use_function_3};
+
+mod function_type;
+
+fn main() {
+    use_function_1();
+    use_function_2();
+    use_function_3();
+}
