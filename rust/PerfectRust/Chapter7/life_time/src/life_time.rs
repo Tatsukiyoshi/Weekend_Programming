e4f8d323fac6ca154822fba1f9b2bcd615ec86a0
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2023-10-14 16:03:41 +0900

    Add: Chapter 7-7 (#772)

--- rust/PerfectRust/Chapter7/life_time/src/life_time.rs
+++ rust/PerfectRust/Chapter7/life_time/src/life_time.rs
@@ -33,10 +33,20 @@ pub fn life_time_3() /* -> &String */ {
     */
 }
 
+
 /// ### リスト7.29 引数の参照を返す関数
 #[allow(dead_code)]
-/*
-fn compare(value1: &String, value2: &String) -> &String {
+pub fn life_time_4(){
+    let a = String::from("ABC");
+    let b = String::from("DEF");
+    let r = compare(&a, &b);
+    println!("compare({:?}, {:?}) = {:?}", a, b, r);
+}
+
+/// ## 7-7 ライフタイム注釈
+/// ### リスト7.30 ライフタイム注釈の利用
+#[allow(dead_code)]
+fn compare<'a>(value1: &'a String, value2: &'a String) -> &'a String {
     // 参照で受け取った２つの文字列の長さを比較する
     if value1.len() > value2.len(){
         value1
@@ -44,14 +54,27 @@ fn compare(value1: &String, value2: &String) -> &String {
         value2
     }
 }
-*/
 
+/// ### リスト7.31 関数実行にも注意
 #[allow(dead_code)]
-pub fn life_time_4(){
-    /*
+pub fn life_time_5(){
+    let r;
     let a = String::from("ABC");
-    let b = String::from("DEF");
-    let r = compare(&a, &b);
-    println!("compare({:?}. {:?} = {:?}", a, b, r);
-    */
+    {
+        let b = String::from("DEFG");
+        r = compare(&a, &b);
+        println!("r = {:?}", r);
+    }
+    // println!("r = {:?}", r);
+}
+
+/// ### リスト7.32 ライフタイム注釈
+/// ### 引数 Vec<i32>のミュータブルな参照
+/// ### 戻り値 Vec<i32>の参照
+#[allow(dead_code)]
+pub fn push(value: &mut Vec<i32>) -> &Vec<i32>{
+    value.push(10);
+    value.push(11);
+    value.push(12);
+    value
 }
