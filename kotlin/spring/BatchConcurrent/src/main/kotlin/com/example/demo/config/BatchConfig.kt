8db19ebcf9d5fda3edf4f4c993a0bc2dd47bc2a9
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2023-10-28 17:32:44 +0900

    Update: Spring Boot 3 (#910)

--- kotlin/spring/BatchConcurrent/src/main/kotlin/com/example/demo/config/BatchConfig.kt
+++ kotlin/spring/BatchConcurrent/src/main/kotlin/com/example/demo/config/BatchConfig.kt
@@ -6,12 +6,13 @@ import com.example.demo.tasklet.ThirdTasklet
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.Step
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
 import org.springframework.batch.core.job.builder.FlowBuilder
+import org.springframework.batch.core.job.builder.JobBuilder
 import org.springframework.batch.core.job.flow.Flow
 import org.springframework.batch.core.job.flow.support.SimpleFlow
 import org.springframework.batch.core.launch.support.RunIdIncrementer
+import org.springframework.batch.core.repository.JobRepository
+import org.springframework.batch.core.step.builder.StepBuilder
 import org.springframework.batch.core.step.tasklet.Tasklet
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
@@ -19,18 +20,11 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.task.SimpleAsyncTaskExecutor
 import org.springframework.core.task.TaskExecutor
+import org.springframework.transaction.PlatformTransactionManager
 
 @Configuration
 @EnableBatchProcessing
 class BatchConfig {
-    /** StepBuilderのFactoryクラス */
-    @Autowired
-    private lateinit var stepBuilderFactory: StepBuilderFactory
-
-    /** JobBuilderのFactoryクラス */
-    @Autowired
-    private lateinit var jobBuilderFactory: JobBuilderFactory
-
     @Autowired
     @Qualifier("FirstTasklet")
     private lateinit var firstTasklet: Tasklet
@@ -45,49 +39,49 @@ class BatchConfig {
 
     /** FirstStepを生成 */
     @Bean
-    fun firstStep(): Step {
-        return stepBuilderFactory.get("FirstStep")
-            .tasklet(firstTasklet)
+    fun firstStep(jobRepository: JobRepository, transactionManager: PlatformTransactionManager): Step {
+        return StepBuilder("FirstStep", jobRepository)
+            .tasklet(firstTasklet, transactionManager)
             .build()
     }
 
     /** SecondStepを生成 */
     @Bean
-    fun secondStep(): Step {
-        return stepBuilderFactory.get("SecondStep")
-            .tasklet(secondTasklet)
+    fun secondStep(jobRepository: JobRepository, transactionManager: PlatformTransactionManager): Step {
+        return StepBuilder("SecondStep", jobRepository)
+            .tasklet(secondTasklet, transactionManager)
             .build()
     }
 
     /** ThirdStepを生成 */
     @Bean
-    fun thirdStep(): Step {
-        return stepBuilderFactory.get("ThirdStep")
-            .tasklet(thirdTasklet)
+    fun thirdStep(jobRepository: JobRepository, transactionManager: PlatformTransactionManager): Step {
+        return StepBuilder("ThirdStep", jobRepository)
+            .tasklet(thirdTasklet, transactionManager)
             .build()
     }
 
     /** FirstStepのFlowを生成 */
     @Bean
-    fun firstFlow(): Flow {
+    fun firstFlow(jobRepository: JobRepository, transactionManager: PlatformTransactionManager): Flow {
         return FlowBuilder<SimpleFlow>("FirstFlow")
-            .start(firstStep()) // Step1をセット
+            .start(firstStep(jobRepository, transactionManager)) // Step1をセット
             .build()            // Flow生成
     }
 
     /** SecondStepのFlowを生成 */
     @Bean
-    fun secondFlow(): Flow {
+    fun secondFlow(jobRepository: JobRepository, transactionManager: PlatformTransactionManager): Flow {
         return FlowBuilder<SimpleFlow>("SecondFlow")
-            .start(secondStep())
+            .start(secondStep(jobRepository, transactionManager))
             .build()
     }
 
     /** ThirdStepのFlowを生成 */
     @Bean
-    fun thirdFlow(): Flow {
+    fun thirdFlow(jobRepository: JobRepository, transactionManager: PlatformTransactionManager): Flow {
         return FlowBuilder<SimpleFlow>("ThirdFlow")
-            .start(thirdStep())
+            .start(thirdStep(jobRepository, transactionManager))
             .build()
     }
 
@@ -99,21 +93,22 @@ class BatchConfig {
 
     /** Flow分割 */
     @Bean
-    fun splitFlow(): Flow {
+    fun splitFlow(jobRepository: JobRepository, transactionManager: PlatformTransactionManager): Flow {
         return FlowBuilder<SimpleFlow>("splitFlow")
             .split(asyncTaskExecutor())     // Flow分割
-            .add(secondFlow(), thirdFlow()) // 同時実行
+            .add(secondFlow(jobRepository, transactionManager),
+                thirdFlow(jobRepository, transactionManager)) // 同時実行
             .build()
     }
 
     /** Jobを生成 */
     @Bean
-    fun concurrentJob(): Job {
-        return jobBuilderFactory.get("ConcurrentJob")
+    fun concurrentJob(jobRepository: JobRepository, transactionManager: PlatformTransactionManager): Job {
+        return JobBuilder("ConcurrentJob", jobRepository)
             .incrementer(RunIdIncrementer())
-                .start(firstFlow()) // 最初のFlow
-                .next(splitFlow())  // 分割したFlowをセット：同時実行
-                .build()            // Flowを生成
-                .build()            // Jobを生成
+            .start(firstFlow(jobRepository, transactionManager)) // 最初のFlow
+            .next(splitFlow(jobRepository, transactionManager))  // 分割したFlowをセット：同時実行
+            .build()            // Flowを生成
+            .build()            // Jobを生成
     }
 }
--- kotlin/spring/BatchPartitioner/src/main/kotlin/com/example/demo/config/BatchConfig.kt
+++ kotlin/spring/BatchPartitioner/src/main/kotlin/com/example/demo/config/BatchConfig.kt
@@ -5,27 +5,21 @@ import com.example.demo.worker.WorkerTasklet
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.Step
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
+import org.springframework.batch.core.job.builder.JobBuilder
 import org.springframework.batch.core.partition.support.Partitioner
+import org.springframework.batch.core.repository.JobRepository
+import org.springframework.batch.core.step.builder.StepBuilder
 import org.springframework.batch.core.step.tasklet.Tasklet
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.task.SimpleAsyncTaskExecutor
 import org.springframework.core.task.TaskExecutor
+import org.springframework.transaction.PlatformTransactionManager
 
 @Configuration
 @EnableBatchProcessing
 class BatchConfig {
-    /** JobBuilderのFactoryクラス */
-    @Autowired
-    private lateinit var jobBuilderFactory: JobBuilderFactory
-
-    /** StepBuilderのFactoryクラス */
-    @Autowired
-    private lateinit var stepBuilderFactory: StepBuilderFactory
-
     @Autowired
     private lateinit var samplePartitioner: Partitioner
 
@@ -40,26 +34,26 @@ class BatchConfig {
 
     /** Stepを生成 */
     @Bean
-    fun workerStep(): Step {
-        return stepBuilderFactory.get("WorkerStep")
-            .tasklet(workerTasklet)
+    fun workerStep(jobRepository: JobRepository, transactionManager: PlatformTransactionManager): Step {
+        return StepBuilder("WorkerStep", jobRepository)
+            .tasklet(workerTasklet, transactionManager)
             .build()
     }
 
     @Bean
-    fun partitionStep(): Step {
-        return stepBuilderFactory.get("PartitionStep")
+    fun partitionStep(jobRepository: JobRepository, transactionManager: PlatformTransactionManager): Step {
+        return StepBuilder("PartitionStep", jobRepository)
             .partitioner("WorkerStep", samplePartitioner)   // Partitioner
-            .step(workerStep()) // step
+            .step(workerStep(jobRepository, transactionManager)) // step
             .gridSize(3)    // 同時実行数
             .taskExecutor(asyncTaskExecutor())
             .build()
     }
 
     @Bean
-    fun partitionJob(): Job {
-        return jobBuilderFactory.get("PartitionJob")
-            .start(partitionStep())
+    fun partitionJob(jobRepository: JobRepository, transactionManager: PlatformTransactionManager): Job {
+        return JobBuilder("PartitionJob", jobRepository)
+            .start(partitionStep(jobRepository, transactionManager))
             .build()
     }
 }
