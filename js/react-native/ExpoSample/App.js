25db9d6c60a4bfc1a0c1f7e5e95f6291a1b617b7
Tatsukiyoshi <48684470+Tatsukiyoshi@users.noreply.github.com>
2023-10-29 15:15:34 +0900

    Update: React-Native 0.72.6

--- js/react-native/ExpoSample/App.js
+++ js/react-native/ExpoSample/App.js
@@ -1,62 +1,20 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-import * as React from 'react';
-import { Text, Button, View, StyleSheet } from 'react-native';
-// import { Constants } from 'expo';
-// https://bagelee.com/programming/react-native/react-native-layout/
-// Expoパッケージを利用せず、デバイスのサイズをもとに算出するように修正
-
-// デバイスのサイズを取得する
-// var {height, width} = Dimensions.get('window');
-
-export default class App extends React.Component {
-  counter = 0;
-
-  constructor(props) {
-    super(props);
-    this.state = {message: 'touch me...'};
-  }
-  // タッチ時の処理
-  doAction = ()=> {
-    this.setState({message: 'count: ' + ++this.counter});
-  }
-
-  render(){
-    return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Expo Sample</Text>
-        <Text style={styles.paragraph}>{this.state.message}</Text>
-        <Button title="Touch me!" onPress={this.doAction} />
-      </View>
-    );
-  }
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
     justifyContent: 'center',
-    // paddingTop: Constants.statusBarHeight,
-    // height: height - 100,
-    backgroundColor: '#ecf0f1',
-    padding: 8,
-  },
-  title: {
-    margin: 20,
-    fontSize: 24,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
   },
 });
