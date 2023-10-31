d2588cfcadbba809182d94ae3d2066887e28dea1
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2023-10-31 22:42:09 +0900

    Update: DDL of Spring Boot 3.0 (#910)

--- kotlin/spring/Database/batch_job_execution.sql
+++ kotlin/spring/Database/batch_job_execution.sql
@@ -4,7 +4,7 @@
 
 CREATE TABLE IF NOT EXISTS public.batch_job_execution
 (
-    job_execution_id bigint NOT NULL,
+    job_execution_id bigint UNIQUE NOT NULL,
     version bigint,
     job_instance_id bigint NOT NULL,
     create_time timestamp without time zone NOT NULL,
@@ -13,13 +13,7 @@ CREATE TABLE IF NOT EXISTS public.batch_job_execution
     status character varying(10) COLLATE pg_catalog."default",
     exit_code character varying(2500) COLLATE pg_catalog."default",
     exit_message character varying(2500) COLLATE pg_catalog."default",
-    last_updated timestamp without time zone,
-    job_configuration_location character varying(2500) COLLATE pg_catalog."default",
-    CONSTRAINT batch_job_execution_pkey PRIMARY KEY (job_execution_id),
-    CONSTRAINT job_inst_exec_fk FOREIGN KEY (job_instance_id)
-        REFERENCES public.batch_job_instance (job_instance_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
+    last_updated timestamp without time zone
 )
 
 TABLESPACE pg_default;
