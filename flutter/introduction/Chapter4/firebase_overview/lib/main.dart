ffbe4deed5533953b99ca6f0cce92ce98c0a8ff5
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2023-11-25 16:56:15 +0900

    Update: Chapter 4 / Section 01 (#812)

--- flutter/introduction/Chapter4/firebase_overview/lib/main.dart
+++ flutter/introduction/Chapter4/firebase_overview/lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -55,71 +62,82 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  // 入力したメールアドレスとパスワード
+  String _email = '';
+  String _password = '';
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Center(
+            child: Container(
+                padding: const EdgeInsets.all(24),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      // 1行目 メールアドレス入力用テキストフィールド
+                      TextFormField(
+                        decoration: const InputDecoration(labelText: 'メールアドレス'),
+                        onChanged: (String value) {
+                          setState(() {
+                            _email = value;
+                          });
+                        },
+                      ),
+                      // 2行目 パスワード入力用テキストフィールド
+                      TextFormField(
+                        decoration: const InputDecoration(labelText: 'パスワード'),
+                        obscureText: true,
+                        onChanged: (String value) {
+                          _password = value;
+                        },
+                      ),
+                      // 3行目 ユーザ登録ボタン
+                      ElevatedButton(
+                        child: const Text('ユーザ登録'),
+                        onPressed: () async {
+                          try {
+                            final User? user = (await FirebaseAuth.instance
+                                    .createUserWithEmailAndPassword(
+                                        email: _email, password: _password))
+                                .user;
+                            if (user != null)
+                              print("ユーザ登録しました ${user.email}, ${user.uid}");
+                          } catch (e) {
+                            print(e);
+                          }
+                        },
+                      ),
+                      // 4行目 ログインボタン
+                      ElevatedButton(
+                          child: const Text('ログイン'),
+                          onPressed: () async {
+                            try {
+                              final User? user = (await FirebaseAuth.instance
+                                      .signInWithEmailAndPassword(
+                                          email: _email, password: _password))
+                                  .user;
+                              if (user != null)
+                                print("ログインしました ${user.email}, ${user.uid}");
+                            } catch (e) {
+                              print(e);
+                            }
+                          }),
+                      // 5行目 パスワードリセット登録ボタン
+                      ElevatedButton(
+                          child: const Text('パスワードリセット'),
+                          onPressed: () async {
+                            try {
+                              await FirebaseAuth.instance
+                                  .sendPasswordResetEmail(email: _email);
+                              print("パスワードリセット用のメールを送信しました");
+                            } catch (e) {
+                              print(e);
+                            }
+                          })
+                    ]))));
   }
 }
