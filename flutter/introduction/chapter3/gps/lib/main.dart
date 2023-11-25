386b102bb8396e3d937240cfe25213fa02217aa0
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2023-11-25 13:26:08 +0900

    Finish: Flutter Introduction Chapter 3 (#954)
    * Add: Chapter 3 / Section 01 Image Picker
    
    * Update: Chapter 3 / Section 01 Image Picker
    
    * Update: Chapter 3 / Section 01 Video Player
    
    * Add: Chapter 3 / Section 02 GPS (#812)
    
    * Add: Chapter 3 / Section 03 Sensor (#812)
    
    * Add: Chapter 3 / Section 04 TTS (#812)
    
    * Add: Chapter 3 / Section 05 STT (#812)

---
+++ flutter/introduction/chapter3/gps/lib/main.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _latitute = "NoData";
+  String _longitute = "NoData";
+  String _altitude = "NoData";
+  String _distanceInMeters = "NoData";
+  String _bearing = "NoData";
+
+  Future<void> getLocation() async {
+    // 権限を獲得
+    LocationPermission permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied ||
+        permission == LocationPermission.deniedForever) {
+      return;
+    }
+    // 位置情報を取得
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    setState(() {
+      // 北緯がプラス、南緯がマイナス
+      _latitute = "緯度： ${position.latitude.toStringAsFixed(2)}";
+      // 東経がプラスで、西経がマイナス
+      _longitute = "経度： ${position.longitude.toStringAsFixed(2)}";
+      // 高度
+      _altitude = "高度： ${position.altitude.toStringAsFixed(2)}";
+      // 距離を1000で割ってkmで返す（サンパウロとの距離）
+      _distanceInMeters =
+          "距離： ${(Geolocator.distanceBetween(position.latitude, position.longitude, -23.61, -46.40) / 1000).toStringAsFixed(2)}";
+      // 方位を返す（サンパウロとの方位）
+      _bearing =
+          "方位： ${(Geolocator.bearingBetween(position.latitude, position.longitude, -23.61, -46.40)).toStringAsFixed(2)}";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(_latitute, style: Theme.of(context).textTheme.headlineMedium),
+            Text(_longitute, style: Theme.of(context).textTheme.headlineMedium),
+            Text(_altitude, style: Theme.of(context).textTheme.headlineMedium),
+            Text(_distanceInMeters,
+                style: Theme.of(context).textTheme.headlineMedium),
+            Text(_bearing, style: Theme.of(context).textTheme.headlineMedium),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getLocation,
+        child: const Icon(Icons.location_on),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ flutter/introduction/chapter3/tts/lib/main.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  FlutterTts flutterTts = FlutterTts();
+  final String _speakText =
+      "寿限無 寿限無 五劫の擦り切れ 海砂利水魚の 水行末 風来末 食う寝る処に住む処 藪ら柑子の藪柑子 パイポパイポ パイポのシューリンガン シューリンガンのグーリンダイ グーリンダイのポンポコピーのポンポコナーの 長久命の長助";
+
+  // 読み上げ用
+  Future<void> _speak() async {
+    await flutterTts.setLanguage("ja-JP"); // 言語
+    await flutterTts.setSpeechRate(1.0); // 速度
+    await flutterTts.setVolume(1.0); // 音量
+    await flutterTts.setPitch(1.0); // ピッチ
+
+    await flutterTts.speak(_speakText);
+  }
+
+  // 停止用
+  Future<void> _stop() async {
+    await flutterTts.stop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              _speakText,
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton:
+          Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+        FloatingActionButton(
+            onPressed: _speak, child: const Icon(Icons.play_arrow)),
+        FloatingActionButton(onPressed: _stop, child: Icon(Icons.stop))
+      ]),
+    );
+  }
+}
