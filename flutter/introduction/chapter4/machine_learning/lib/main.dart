5eaecea4910582c868a7c7107ce9ecce7d9338a5
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2023-12-09 12:01:00 +0900

    Add: Chapter 4 / Section 06 (#812)

---
+++ flutter/introduction/chapter4/machine_learning/lib/main.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'dart:async';
+import 'dart:io';
+import 'firebase_options.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+import 'package:google_mlkit_image_labeling/google_mlkit_image_labeling.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Text? _text;
+  Image? _image;
+
+  Future<void> _ocr() async {
+    final pickerFile =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickerFile == null) {
+      return;
+    }
+    final InputImage imageFile = InputImage.fromFilePath(pickerFile.path);
+    final textRecognizer =
+        TextRecognizer(script: TextRecognitionScript.japanese);
+    final RecognizedText recognizedText =
+        await textRecognizer.processImage(imageFile);
+    String text = recognizedText.text;
+
+    setState(() {
+      _text = Text(text);
+      _image = Image.file(File(pickerFile.path));
+    });
+
+    // リソースの解放
+    textRecognizer.close();
+  }
+
+  // ラベリングを行う
+  Future<void> _labeling() async {
+    final pickerFile =
+        await ImagePicker().pickImage(source: ImageSource.camera);
+    if (pickerFile == null) {
+      return;
+    }
+    final InputImage imageFile = InputImage.fromFilePath(pickerFile.path);
+    final ImageLabelerOptions options =
+        ImageLabelerOptions(confidenceThreshold: 0.7);
+    final imageLabeler = ImageLabeler(options: options);
+    final List<ImageLabel> labels = await imageLabeler.processImage(imageFile);
+
+    String text = "";
+    for (ImageLabel label in labels) {
+      text +=
+          "${label.label} (${(label.confidence * 100).toStringAsFixed(0)}%)\n";
+    }
+
+    // 画面に反映
+    setState(() {
+      _text = Text(text);
+      _image = Image.file(File(pickerFile.path));
+    });
+
+    // リソースの解放
+    imageLabeler.close();
+    return;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              if (_text != null) _text!,
+              if (_image != null) SafeArea(child: _image!),
+            ],
+          ),
+        ),
+        floatingActionButton:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          FloatingActionButton(
+              onPressed: _ocr, child: const Icon(Icons.photo_album)),
+          FloatingActionButton(
+              onPressed: _labeling, child: const Icon(Icons.photo_camera))
+        ]));
+  }
+}
