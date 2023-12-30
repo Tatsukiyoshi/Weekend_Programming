385741b1d657512d0d63d565a93db03ebf473c8b
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2023-12-30 19:18:48 +0900

    Finish: Perfect Rust Chapter 17 (#1010)
    * Add: Chapter 17-2 (#772)
    
    * Add: Chapter 17-3 (#772)
    
    * Add: Chapter 17-4 (#772)
    
    * Add: Chapter 17-5 (#772)
    
    * Add: Chapter 17-6 (#772)
    
    * Add: Chapter 17-7 (#772)

---
+++ rust/PerfectRust/Chapter17/actix_web_sample/src/handlers/tera_handler.rs
@@ -0,0 +1,121 @@
+use actix_web::{error, HttpResponse, Responder, web};
+use serde::{Deserialize, Serialize};
+use tera::Tera;
+
+/// ## 17-6 Teraクレート
+/// ### リスト17.20 リクエストパラメータ用構造体
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CalcForm {
+    pub value1: Option<String>,
+    pub value2: Option<String>,
+    pub opt: Option<String>
+}
+impl CalcForm {
+    pub fn calc(&self) -> anyhow::Result<i32> {
+        let func = |v: &String| -> i32 {
+            if v.eq("") {
+                return 0;
+            } else {
+                v.parse::<i32>().unwrap()
+            }
+        };
+        let value1 = self.value1.as_ref().map_or_else(|| 0, |v|{ func(v) });
+        let value2 = self.value2.as_ref().map_or_else(|| 0, |v|{ func(v) });
+        let opt = self.opt.as_ref().map_or_else(|| 0, |v|{ func(v) });
+        let result = match opt {
+            1 => value1 + value2,
+            2 => value1 - value2,
+            3 => value1 * value2,
+            4 => value1 / value2,
+            5 => value1 % value2,
+            _ => return Err(anyhow::Error::msg("Parameter Error."))
+        };
+        Ok(result)
+    }
+}
+/// ### リスト17.19 GETリクエストへの応答
+pub async fn calc_get(tera: web::Data<Tera>) -> impl Responder {
+    let resp_body = tera.render(
+        "pages/enter.html", &tera::Context::new())
+        .map_err(|err| error::ErrorInternalServerError(err.to_string())).unwrap();
+    HttpResponse::Ok().content_type(mime::TEXT_HTML).body(resp_body)
+}
+
+/// ### リスト17.23 POSTリクエストへの応答
+pub async fn calc_post(form: web::Form<CalcForm>, tera: web::Data<Tera>) -> impl Responder {
+    let mut context = tera::Context::new();
+    let calc_form = form.into_inner();
+    // 計算した結果をcontextに格納する
+    match calc_form.calc() {
+        Ok(result) => context.insert("result", &result),
+        Err(err) => context.insert("result", &err.to_string())
+    }
+    // HTMLコンテンツを取得する
+    let resp_body = tera.render(
+        "pages/result.html", &context)
+        .map_err(|err| error::ErrorInternalServerError(err.to_string())).unwrap();
+    HttpResponse::Ok().content_type(mime::TEXT_HTML).body(resp_body)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::tera_handler::CalcForm;
+    use http::StatusCode;
+    use actix_web::dev::ServiceResponse;
+    use actix_web::{App, Error, test, web};
+    use actix_web::web::resource;
+    use tera::Tera;
+
+    /// ## 17-7 リクエストハンドラのテスト
+    /// ### リスト17.24 テスト用サービスの生成
+    async fn test_service() -> impl actix_web::dev::Service<actix_http::Request,
+        Response = ServiceResponse, Error = Error> {
+        // teraを生成する
+        let tera = Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/views/**/*")).unwrap();
+        // テスト用サービスを生成する
+        let test_service = test::init_service(
+            App::new()
+                .app_data(web::Data::new(tera.clone()))
+                .service(
+                    web::scope("/sample")
+                        .service(
+                            resource("/calc_form")
+                            .route(web::get().to(super::calc_get))
+                            .route(web::post().to(super::calc_post)))
+                )
+        ).await;
+        test_service
+    }
+
+    /// ### リスト17.25 GETリクエストのテスト
+    #[actix_web::test]
+    async fn test_get_request(){
+        let test_service = test_service().await;
+        let enter_request = test::TestRequest::get().uri("/sample/calc_form").to_request();
+        // リクエストハンドラenter()を実行する
+        let response = test::call_service(&test_service, enter_request).await;
+        println!("{:?}", response.headers());
+        println!("{:?}", response.response().body());
+        assert_eq!(response.status(), StatusCode::OK);
+    }
+
+    /// ### リスト17.25 POSTリクエストのテスト
+    #[actix_web::test]
+    async fn test_post_request() -> () {
+        let test_service = test_service().await;
+        // 入力データを準備する
+        let calc_form = CalcForm{
+            value1: Some("100".to_owned()),
+            value2: Some("200".to_owned()),
+            opt: Some("1".to_owned())
+        };
+        // CalcFormを格納したPostリクエストを生成する
+        let answer_request = test::TestRequest::post()
+            .uri("/sample/calc_form").set_form(&calc_form).to_request();
+        // リクエストハンドラcalc_post()を実行する
+        let response = test::call_service(&test_service, answer_request).await;
+        println!("{:?}", response.headers());
+        println!("{:?}", response.response().body());
+        assert_eq!(response.status(), StatusCode::OK);
+    }
+}
---
+++ rust/PerfectRust/Chapter17/actix_web_sample/src/main.rs
@@ -0,0 +1,70 @@
+mod handlers;
+mod pool;
+
+use actix_web::{App, HttpServer, middleware, web};
+use actix_web::web::{resource, ServiceConfig};
+use tera::Tera;
+use pool::SamplePool;
+use handlers::{handler_func, tera_handler};
+
+/// ## 17-2.アプリケーションの基本構造
+/// ### リスト17.2 main()関数の基本構造
+/// ### リスト17.3 データとミドルウェアの追加
+/// ### リスト17.14 コネクションプールをData型で登録
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // ロガーを初期化する
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    // Template Endine Teraを生成する
+    let tera = Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/views/**/*")).unwrap();
+    // SeaORMのコネクションプールの登録（16章参照）
+    let pool = SamplePool::get().await.expect("接続プールを取得できません");
+    // HttpServerの起動
+    // ### リスト17.6 route()メソッドを利用したマッピング
+    // ### リスト17.7 scope()関数とroute()メソッドを利用したマッピング
+    // ### リスト17.8 resource()関数とroute()メソッドを利用したマッピング
+    // ### リスト17.9 configure()メソッドの利用
+    HttpServer::new(move || {
+        App::new()
+            .wrap(middleware::Logger::default())    // ロギングミドルウェアの登録
+            .app_data(web::Data::new(tera.clone())) // Teraの登録
+            .app_data(web::Data::new(pool.clone())) // SeaORMのコネクションプールの登録
+            .configure(set_configure)   // パスとハンドラのマッピング
+    }).bind("127.0.0.1:8080")?.run().await
+}
+
+/// ### リスト17.9 configure()メソッドの利用
+fn set_configure(cfg: &mut ServiceConfig) -> () {
+    cfg.service(
+        web::scope("/sample")
+            .service(handler_func::calc_1)
+            .service(
+                resource("/calc2")
+                    .route(web::get().to(handler_func::calc_2))
+                    .route(web::post().to(handler_func::calc_2))
+            )
+            .service(
+                resource("/calc3")
+                    .route(web::get().to(handler_func::calc_3))
+                    .route(web::post().to(handler_func::calc_3))
+            )
+            .service(
+                resource("/calc4")
+                    .route(web::get().to(handler_func::calc_4))
+                    .route(web::post().to(handler_func::calc_4))
+            )
+            .service(
+                resource("/usepool")
+                    .route(web::get().to(handler_func::use_pool))
+                    .route(web::post().to(handler_func::use_pool))
+            )
+            .service(
+                resource("/calc5")
+                    .route(web::get().to(handler_func::calc_5))
+                    .route(web::post().to(handler_func::calc_5))
+            )
+            .service(resource("/calc_form")
+                .route(web::get().to(tera_handler::calc_get))
+                .route(web::post().to(tera_handler::calc_post)))
+    );
+}
