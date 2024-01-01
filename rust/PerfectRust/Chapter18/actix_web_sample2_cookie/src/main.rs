7e6173ccd849cc77d2297bec9b1ed2af71e2116d
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2024-01-01 20:25:05 +0900

    Add: Chapter 18-3 (#772)

--- rust/PerfectRust/Chapter18/actix_web_sample2_cookie/src/main.rs
+++ rust/PerfectRust/Chapter18/actix_web_sample2_cookie/src/main.rs
@@ -10,6 +10,7 @@ use actix_web::cookie::time::Duration;
 use actix_session::config::BrowserSession;
 use actix_session::SessionMiddleware;
 use actix_session::storage::CookieSessionStore;
+use openssl::ssl::{SslAcceptor, SslAcceptorBuilder, SslFiletype, SslMethod};
 
 /// ## 18-1 actix-sessionクレート
 /// ### リスト18.1 CookieSessionStoreを利用するSessionMiddlewareの生成と登録
@@ -31,5 +32,20 @@ async fn main() -> std::io::Result<()>{
                     )
                     .cookie_name("rsessionid".to_string()).build()
             )
-    }).bind("127.0.0.1:8080")?.run().await
+    }).bind_openssl("127.0.0.1:8080", create_ssl_acceptor_builder())?.run().await
+}
+
+/// ## 18-3 opensslクレート
+/// ### リスト18.12 SslAcceptorBuilderの生成と機密鍵、シークレットキーの設定
+
+// OpenSSL SslAcceptorBuilderの生成
+fn create_ssl_acceptor_builder() -> SslAcceptorBuilder {
+    // OpenSSL構造を管理し、暗号スイート、セッションオプションなどを構成する
+    let mut builder: SslAcceptorBuilder =
+    SslAcceptor::mozilla_intermediate_v5(SslMethod::tls_server()).unwrap();
+    // 秘密鍵の設定
+    builder.set_private_key_file("local-key.pem", SslFiletype::PEM).unwrap();
+    // 証明書の設定
+    builder.set_certificate_chain_file("local.pem").unwrap();
+    builder
 }
