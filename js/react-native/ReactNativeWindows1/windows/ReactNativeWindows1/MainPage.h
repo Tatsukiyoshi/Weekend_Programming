090081f8b25edee9857ccf6f14456e7e3da4eacf
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2024-01-07 02:51:39 +0900

    Add: Sample of React-Native-Windows (#860)

---
+++ js/react-native/ReactNativeWindows1/windows/ReactNativeWindows1/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::ReactNativeWindows1::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::ReactNativeWindows1::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
