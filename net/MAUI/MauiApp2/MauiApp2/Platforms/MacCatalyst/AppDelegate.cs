7705b6e63634816031b352cd9ff6657f88e316f2
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2024-04-12 22:04:06 +0900

    Trial: MAUI on Visual Studio 2022 (#1574) (#1577)

--- net/MAUI/MauiApp2/MauiApp2/Platforms/MacCatalyst/AppDelegate.cs
+++ net/MAUI/MauiApp2/MauiApp2/Platforms/MacCatalyst/AppDelegate.cs
@@ -1,9 +1,10 @@
 ﻿using Foundation;
 
-namespace MauiApp2;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
+namespace MauiApp2
 {
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
 }
