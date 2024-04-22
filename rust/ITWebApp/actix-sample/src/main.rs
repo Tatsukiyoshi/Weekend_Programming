c13e235570632da73b042d1cc02db1fbd1d5c424
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2024-04-22 17:30:02 +0900

    Add: WebApp @IT Vol. 3 (#1601)

--- rust/ITWebApp/actix-sample/src/main.rs
+++ rust/ITWebApp/actix-sample/src/main.rs
@@ -2,9 +2,10 @@
 mod handler;
 
 use std::io::Result;
-use actix_web::{App, HttpServer, Responder, HttpResponse, get, 
-	middleware::Logger};
+use actix_web::{App, HttpServer, Responder, HttpResponse, get, web,
+				middleware::Logger};
 use env_logger::Env;
+use crate::handler::{create, index, new, show};
 
 #[actix_rt::main]
 async fn main() -> Result<()> {
@@ -12,7 +13,11 @@ async fn main() -> Result<()> {
 	env_logger::init_from_env(Env::default().default_filter_or("info"));
 	HttpServer::new(|| {
 		App::new()
-			.service(handler::index)	// handler登録
+			.service(index)	// handler登録
+			.service(new)
+			.service(create)
+			.service(show)
+			.default_service(web::to(handler::not_found))
 			.wrap(Logger::default())	// Loggerの追加
 	})
 	.bind("127.0.0.1:8000")?.run().await
