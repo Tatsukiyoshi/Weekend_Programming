c437f8053d06db8808c8f19d07a02834a45d0fa2
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2024-04-23 14:55:48 +0900

    Add: WebApp @IT Vol.4 (#1605)

--- rust/ITWebApp/actix-sample/src/handler.rs
+++ rust/ITWebApp/actix-sample/src/handler.rs
@@ -4,6 +4,7 @@ use log::info;
 use actix_web::{Responder, HttpResponse, web, get, post};
 use serde::{Serialize, Deserialize};
 use chrono::{DateTime, Local, Duration};
+use tera::Context;
 
 /// ##  デフォルトページ
 pub async fn not_found() -> impl Responder {
@@ -12,61 +13,37 @@ pub async fn not_found() -> impl Responder {
 
 /// ##  一覧表示
 #[get("/posts")]
-pub async fn index() -> impl Responder {
+pub async fn index(tmpl: web::Data<tera::Tera>) -> impl Responder {
+  info!("Called index");
   let posts = data::get_all();
-  let mut body_str: String = "".to_string();
-
-  body_str += include_str!("../static/header.html");
-
-  for item in &posts {
-    body_str += &format!("<div><a href=\"/posts/{}\">", item.id);
-    body_str += &format!("<div>{} {}</div>", item.sender, item.posted);
-    body_str += &format!("<div><p>{}</p></div>", 
-      item.content.replace("\n", "<br />"));
-    body_str += "</a></div>";
-  }
-  body_str += "<div><a href=\"/posts/new\">作成</a></div>";
-  body_str += include_str!("../static/footer.html");
+  let mut context = Context::new();
+  context.insert("posts", &posts);
+  let body_str = tmpl.render("index.html", &context).unwrap();
   HttpResponse::Ok().content_type("text/html; charset=utf-8").body(body_str)
 }
 
 /// ##  投稿表示
 #[get("/posts/{id}")]
-pub async fn show(info: web::Path<i32>) -> impl Responder {
+pub async fn show(tmpl: web::Data<tera::Tera>, info: web::Path<i32>) -> impl Responder {
   info!("Called show");
   let info = info.into_inner();
   let post = data::get(info);
-  let mut body_str: String = "".to_string();
-  body_str += include_str!("../static/header.html");
-  body_str += "<div>";
-  if post.id != 0 {
-    body_str += &format!("<div>投稿者：{}</div>", post.sender);
-    body_str += &format!("<div>投稿日時：{}</div>", post.posted);
-    body_str += &format!("<div>投稿内容：<br />{}</div>", post.content.replace("\n", "<br />"));
-    body_str += &format!("<div><a href=\"/posts/{}/edit\">編集</a> ", info);
-    body_str += &format!("<a href=\"/posts/{}/delete\">削除</a><div>", info);
-  } else {
-    body_str += "見つかりません。";
-  }
-  body_str += "</div>";
-  body_str += "<div><a href=\"/posts\">一覧へ</a></div>";
-  body_str += include_str!("../static/footer.html");
+  let mut context = Context::new();
+  context.insert("post", &post);
+  let body_str = tmpl.render("show.html", &context).unwrap();
   HttpResponse::Ok().content_type("text/html; charset=utf-8").body(body_str)
 }
 
 /// ##  投稿フォーム呼び出し
 #[get("/posts/new")]
-pub async fn new() -> impl Responder {
-  let mut body_str: String = "".to_string();
-  body_str += include_str!("../static/header.html");
-  body_str += include_str!("../static/form.html");
-  body_str += include_str!("../static/footer.html");
-  body_str = body_str.replace("{{action}}", "create");
-  body_str = body_str.replace("{{id}}", "0");
-  body_str = body_str.replace("{{posted}}", "");
-  body_str = body_str.replace("{{sender}}", "");
-  body_str = body_str.replace("{{content}}", "");
-  body_str = body_str.replace("{{button}}", "登録");
+pub async fn new(tmpl: web::Data<tera::Tera>) -> impl Responder {
+  info!("Called new");
+  let mut context = Context::new();
+  let post = data::Message {id:0, sender:"".to_string(), content:"".to_string(), posted:"".to_string()};
+  context.insert("action", "create");
+  context.insert("post", &post);
+  context.insert("button", "投稿");
+  let body_str = tmpl.render("form.html", &context).unwrap();
   HttpResponse::Ok().content_type("text/html; charset=utf-8").body(body_str)
 }
 
@@ -79,7 +56,7 @@ pub struct CreateForm {
   content: String,
 }
 
-/// ##  登録実行関数
+/// ##  投稿登録
 #[post("/posts/create")]
 pub async fn create(params: web::Form<CreateForm>) -> impl Responder {
   let now: DateTime<Local> = Local::now();
@@ -91,4 +68,42 @@ pub async fn create(params: web::Form<CreateForm>) -> impl Responder {
   };
   message = data::create(message);
   web::Redirect::to(format!("/posts/{}", message.id)).see_other()
+}
+
+/// ##  編集モードでの呼び出し
+#[get("/posts/{id}/edit")]
+pub async fn edit(tmpl: web::Data<tera::Tera>, info: web::Path<i32>) -> impl Responder {
+  info!("Called edit");
+  let info = info.into_inner();
+  let post = data::get(info);
+  let mut context = Context::new();
+  context.insert("action", "update");
+  context.insert("post", &post);
+  context.insert("button", "更新");
+  let body_str = tmpl.render("form.html", &context).unwrap();
+  HttpResponse::Ok().content_type("text/html; charset=utf-8").body(body_str)
+}
+
+/// ##  投稿更新
+#[post("/posts/update")]
+pub async fn update(params: web::Form<CreateForm>) -> impl Responder {
+  info!("Called update");
+  let message = data::Message {
+    id: params.id,
+    posted: params.posted.clone(),
+    sender: params.sender.clone(),
+    content: params.content.clone()
+  };
+  data::update(&message);
+  web::Redirect::to(format!("/posts/{}", message.id)).see_other()
+  //web::Redirect::to("/posts").see_other()
+}
+
+/// ##  投稿削除
+#[get("/posts/{id}/delete")]
+pub async fn destroy(info: web::Path<i32>) -> impl Responder {
+  info!("Called destroy");
+  let info = info.into_inner();
+  data::remove(info);
+  web::Redirect::to("/posts").see_other()
 }
