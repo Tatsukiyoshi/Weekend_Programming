c1b40a5e791620153762c6a27f6493961ca4bfa9
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2024-04-23 18:42:25 +0900

    Add: WebApp @IT Vol.5 (#1601) (#1606)
    * Add: WebApp @IT Vol.5 (#1601)
    
    * Update: WebApp @IT Vol.5 (#1601)

--- rust/ITWebApp/actix-sample/src/handler.rs
+++ rust/ITWebApp/actix-sample/src/handler.rs
@@ -2,9 +2,13 @@ mod data;
 
 use log::info;
 use actix_web::{Responder, HttpResponse, web, get, post};
+use actix_session::Session;
 use serde::{Serialize, Deserialize};
 use chrono::{DateTime, Local, Duration};
 use tera::Context;
+use actix_web_flash_messages::{
+  FlashMessage, IncomingFlashMessages, Level,
+};
 
 /// ##  デフォルトページ
 pub async fn not_found() -> impl Responder {
@@ -13,10 +17,19 @@ pub async fn not_found() -> impl Responder {
 
 /// ##  一覧表示
 #[get("/posts")]
-pub async fn index(tmpl: web::Data<tera::Tera>) -> impl Responder {
+pub async fn index(tmpl: web::Data<tera::Tera>, messages: IncomingFlashMessages)
+    -> impl Responder {
   info!("Called index");
   let posts = data::get_all();
   let mut context = Context::new();
+
+  for message in messages.iter() {
+    match message.level() {
+      Level::Success => context.insert("success", &message.content()),
+      Level::Error => context.insert("error", &message.content()),
+      _ => (),
+    }
+  }
   context.insert("posts", &posts);
   let body_str = tmpl.render("index.html", &context).unwrap();
   HttpResponse::Ok().content_type("text/html; charset=utf-8").body(body_str)
@@ -36,10 +49,17 @@ pub async fn show(tmpl: web::Data<tera::Tera>, info: web::Path<i32>) -> impl Res
 
 /// ##  投稿フォーム呼び出し
 #[get("/posts/new")]
-pub async fn new(tmpl: web::Data<tera::Tera>) -> impl Responder {
+pub async fn new(tmpl: web::Data<tera::Tera>, session: Session) -> impl Responder {
   info!("Called new");
   let mut context = Context::new();
-  let post = data::Message {id:0, sender:"".to_string(), content:"".to_string(), posted:"".to_string()};
+  // 投稿者名をセッションから取得
+  let mut sender = "".to_string();
+  if let Some(s) = session.get::<String>("sender").unwrap() {
+    sender = s;
+  } else {
+    sender = "名無しさん".to_string();
+  }
+  let post = data::Message {id:0, sender:sender, content:"".to_string(), posted:"".to_string()};
   context.insert("action", "create");
   context.insert("post", &post);
   context.insert("button", "投稿");
@@ -58,7 +78,8 @@ pub struct CreateForm {
 
 /// ##  投稿登録
 #[post("/posts/create")]
-pub async fn create(params: web::Form<CreateForm>) -> impl Responder {
+pub async fn create(params: web::Form<CreateForm>, session: Session) -> impl Responder {
+  info!("Called create");
   let now: DateTime<Local> = Local::now();
   let mut message = data::Message {
     id: 0,
@@ -67,6 +88,12 @@ pub async fn create(params: web::Form<CreateForm>) -> impl Responder {
     content: params.content.clone()
   };
   message = data::create(message);
+  if message.id == 0 {
+    FlashMessage::error("投稿でエラーが発生しました。").send();
+  } else {
+    FlashMessage::success("投稿しました。").send();
+  }
+  let _ = session.insert("sender", params.sender.clone());  //  投稿者名をセッションに保存
   web::Redirect::to(format!("/posts/{}", message.id)).see_other()
 }
 
@@ -95,6 +122,7 @@ pub async fn update(params: web::Form<CreateForm>) -> impl Responder {
     content: params.content.clone()
   };
   data::update(&message);
+  FlashMessage::success("更新しました。").send();
   web::Redirect::to(format!("/posts/{}", message.id)).see_other()
   //web::Redirect::to("/posts").see_other()
 }
@@ -105,5 +133,7 @@ pub async fn destroy(info: web::Path<i32>) -> impl Responder {
   info!("Called destroy");
   let info = info.into_inner();
   data::remove(info);
+  FlashMessage::success("削除しました。").send();
   web::Redirect::to("/posts").see_other()
 }
+
--- rust/ITWebApp/actix-sample/src/main.rs
+++ rust/ITWebApp/actix-sample/src/main.rs
@@ -4,15 +4,33 @@ mod handler;
 use std::io::Result;
 use actix_web::{App, HttpServer, Responder, HttpResponse, get, web,
 				middleware::Logger};
+use actix_web::cookie::{Key};
+use actix_session::storage::CookieSessionStore;
+use actix_session::SessionMiddleware;
+use actix_web_flash_messages::FlashMessagesFramework;
+use actix_web_flash_messages::storage::SessionMessageStore;
+use actix_web_flash_messages::storage::CookieMessageStore;
 use env_logger::Env;
 use crate::handler::{create, destroy, edit, index, new, show, update};
 use tera::Tera;
 
+fn build_cookie_session_middleware(key: Key)
+								   -> SessionMiddleware<CookieSessionStore> {
+	SessionMiddleware::builder(CookieSessionStore::default(), key).build()
+}
+
 #[actix_rt::main]
 async fn main() -> Result<()> {
-	// ログレベルの設定
+	//	ログレベルの設定
 	env_logger::init_from_env(Env::default().default_filter_or("info"));
-	HttpServer::new(|| {
+	//	クッキーのキー生成
+	let key = Key::generate();
+	//	メッセージストアおよびフレームワークインスタンス生成
+	// let message_store = SessionMessageStore::default();
+	// メッセージストアにクッキーを使う場合
+	let message_store = CookieMessageStore::builder(key.clone()).build();
+	let message_framework = FlashMessagesFramework::builder(message_store).build();
+	HttpServer::new(move || {
 		let tera = Tera::new("templates/**/*.html").unwrap();
 		App::new()
 			.app_data(web::Data::new(tera))
@@ -25,6 +43,8 @@ async fn main() -> Result<()> {
 			.service(destroy)
 			.default_service(web::to(handler::not_found))
 			.wrap(Logger::default())	// Loggerの追加
+			.wrap(message_framework.clone())
+			.wrap(build_cookie_session_middleware(key.clone()))
 	})
 	.bind("127.0.0.1:8000")?.run().await
 }
