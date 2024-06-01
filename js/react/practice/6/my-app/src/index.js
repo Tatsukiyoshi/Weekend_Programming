72ea79814971947640155ec584cfa72a4abe106f
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2024-06-01 23:56:47 +0900

    Fix: Chapter 6 to React 18.3.1 (#1660)

--- js/react/practice/6/my-app/src/index.js
+++ js/react/practice/6/my-app/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-// Switch to React 18 from React 17
-//import { createRoot } from 'react-dom/client';
-import './index.css';
-import { App } from './App';
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-// Switch to React 18 from React 17
-//const container = document.getElementById(App);
-//const root = createRoot(container);
-//root.render(<App tab="home" />);
+const root = ReactDOM.createRoot(
+    document.getElementById('root')
+);
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
