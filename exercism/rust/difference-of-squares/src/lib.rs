c0ad41f49f5343cd2cccd14b1389f4f62720f6e4
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2024-06-26 15:25:03 +0900

    Add: Tested Difference-of-Squares (#1676)

--- exercism/rust/difference-of-squares/src/lib.rs
+++ exercism/rust/difference-of-squares/src/lib.rs
@@ -1,11 +1,22 @@
+/// 1からnまでの合計の２乗を返す
 pub fn square_of_sum(n: u32) -> u32 {
-    todo!("square of sum of 1...{n}")
+    let mut sum = 0;
+    for i in 1..(n + 1){
+        sum = sum + i;
+    }
+    sum.pow(2)
 }
 
-pub fn sum_of_squares(n: u32) -> u32 {
-    todo!("sum of squares of 1...{n}")
+/// １からｎまでをそれぞれ２乗した値の合計を返す
+pub fn sum_of_squares(n: u32) -> u32
+{
+    let mut sum = 0;
+    for i in 1..(n + 1){
+        sum = sum + i.pow(2);
+    }
+    sum
 }
 
 pub fn difference(n: u32) -> u32 {
-    todo!("difference between square of sum of 1...{n} and sum of squares of 1...{n}")
+    square_of_sum(n) - sum_of_squares(n)
 }
