a9f7f1336c33fc1e73a40b7542f52a371d480d66
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2024-06-27 14:48:38 +0900

    Add: Tested Proverb (#1676)

--- exercism/rust/proverb/src/lib.rs
+++ exercism/rust/proverb/src/lib.rs
@@ -1,3 +1,18 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    todo!("build a proverb from this list of items: {list:?}")
+    let mut rhyme: String = "".to_string();
+
+    if list.len() > 0 {
+        for i in 0..list.len() - 1{
+            rhyme.push_str("For want of a ");
+            rhyme.push_str(list[i]);
+            rhyme.push_str(" the ");
+            rhyme.push_str(list[i + 1]);
+            rhyme.push_str(" was lost.\n");
+        }
+        rhyme.push_str("And all for the want of a ");
+        rhyme.push_str(list[0]);
+        rhyme.push_str(".");
+    }
+
+    rhyme
 }
