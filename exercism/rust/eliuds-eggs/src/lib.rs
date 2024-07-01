25b8a72e4ec7596e59978bcdffb348e37586297a
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2024-07-01 10:09:21 +0900

    Add: Tested Eliuds-Eggs (#1676)

--- exercism/rust/eliuds-eggs/src/lib.rs
+++ exercism/rust/eliuds-eggs/src/lib.rs
@@ -1,3 +1,16 @@
+/// 与えられた数を2進数に変換する中でビットが１であれば、
+/// 卵があるという判定で卵の数をインクリメントする
 pub fn egg_count(display_value: u32) -> usize {
-    todo!("count the eggs in {display_value}")
+    let mut dividend = display_value;
+    let mut eggs = 0;
+    loop {
+        if dividend % 2 == 1 {
+            eggs = eggs + 1;
+        }
+        if dividend < 2 {
+            break;
+        }
+        dividend = dividend / 2;
+    }
+    eggs
 }
