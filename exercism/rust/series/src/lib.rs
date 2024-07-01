1d7a873e3fa38c5c1560f4cb49ec6f9ca5ea32ae
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2024-07-01 17:57:49 +0900

    Add: Tested Series (#1676)

--- exercism/rust/series/src/lib.rs
+++ exercism/rust/series/src/lib.rs
@@ -1,3 +1,18 @@
+/// 入力された数字列の先頭から順に、指定された桁数の数字列を切り出し、
+/// 切り出された複数の数字列をベクタ配列で返す
 pub fn series(digits: &str, len: usize) -> Vec<String> {
-    todo!("What are the series of length {len} in string {digits:?}")
+    let mut results_count = 1;
+    if len <= digits.len() {
+        results_count = digits.len() - len + 1;
+    }
+    let mut result: Vec<String> = Vec::with_capacity(results_count);
+
+    for i in 0..results_count {
+        if len <= digits.len() {
+            let str = digits[i..(i + len)].to_string();
+            result.push(str);
+        }
+    }
+
+    result
 }
