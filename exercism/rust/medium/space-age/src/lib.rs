17bd5d4a5753115d993776199faa4e1c1ad11541
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2024-07-10 14:11:51 +0900

    Add: Tested Space Age (#1676)

--- exercism/rust/medium/space-age/src/lib.rs
+++ exercism/rust/medium/space-age/src/lib.rs
@@ -2,17 +2,22 @@
 // In order to pass the tests you can add-to or change any of this code.
 
 #[derive(Debug)]
-pub struct Duration;
+pub struct Duration {
+    secs: u64
+}
 
 impl From<u64> for Duration {
-    fn from(s: u64) -> Self {
-        todo!("s, measured in seconds: {s}")
+    fn from(s: u64) -> Self
+    {
+        Duration {secs: s}
     }
 }
 
 pub trait Planet {
+    const YEARS_ON_SECONDS: f64 = 31557600.00;
+    const CONVERSION_TO_EARTH_YEARS: f64;
     fn years_during(d: &Duration) -> f64 {
-        todo!("convert a duration ({d:?}) to the number of years on this planet for that duration");
+        d.secs as f64 / Self::YEARS_ON_SECONDS / Self::CONVERSION_TO_EARTH_YEARS
     }
 }
 
@@ -25,11 +30,42 @@ pub struct Saturn;
 pub struct Uranus;
 pub struct Neptune;
 
-impl Planet for Mercury {}
-impl Planet for Venus {}
-impl Planet for Earth {}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+/// 水星
+impl Planet for Mercury {
+    const CONVERSION_TO_EARTH_YEARS: f64 = 0.2408467;
+}
+
+/// 金星
+impl Planet for Venus {
+    const CONVERSION_TO_EARTH_YEARS: f64 = 0.61519726;
+}
+
+/// 地球
+impl Planet for Earth {
+    const CONVERSION_TO_EARTH_YEARS: f64 = 1.0;
+}
+
+/// 火星
+impl Planet for Mars {
+    const CONVERSION_TO_EARTH_YEARS: f64 = 1.8808158;
+}
+
+/// 木星
+impl Planet for Jupiter {
+    const CONVERSION_TO_EARTH_YEARS: f64 = 11.862615;
+}
+
+/// 土星
+impl Planet for Saturn {
+    const CONVERSION_TO_EARTH_YEARS: f64 = 29.447498;
+}
+
+/// 天王星
+impl Planet for Uranus {
+    const CONVERSION_TO_EARTH_YEARS: f64 = 84.016846;
+}
+
+/// 海王星
+impl Planet for Neptune {
+    const CONVERSION_TO_EARTH_YEARS: f64 = 164.79132;
+}
