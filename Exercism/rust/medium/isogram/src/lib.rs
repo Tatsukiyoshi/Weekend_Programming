2a5db0ef9c8544dfeea76a56fbc5ee249282685e
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2024-07-18 17:35:26 +0900

    Add: Tested Isogram (#1676)

--- Exercism/rust/medium/isogram/src/lib.rs
+++ Exercism/rust/medium/isogram/src/lib.rs
@@ -1,3 +1,20 @@
+/// 出現済みの文字が出現したら、isogramではないと判定する
+/// なお、大小文字は等価とみなす
 pub fn check(candidate: &str) -> bool {
-    todo!("Is {candidate} an isogram?");
+    let mut read: String = "".to_string();
+    let input = candidate.chars();
+
+    for char in input {
+        println!("{}", char);
+        if char == '-' || char == ' ' {
+            continue;
+        }
+        let schar = char.to_ascii_lowercase();
+        if read.find(schar) == None {
+            read.push(schar);
+        } else {
+            return false;
+        }
+    }
+    true
 }
