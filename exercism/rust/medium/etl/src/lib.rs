f4a18c37e96faea7fbae4ee8a45625ef6064eaa9
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2024-07-18 11:56:54 +0900

    Add: Tested ETL (#1676)

--- exercism/rust/medium/etl/src/lib.rs
+++ exercism/rust/medium/etl/src/lib.rs
@@ -1,5 +1,11 @@
 use std::collections::BTreeMap;
 
 pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
-    todo!("How will you transform the tree {h:?}?")
+    let mut outputs: BTreeMap<char, i32> = BTreeMap::new();
+    for map in h {
+        for word in map.1 {
+            outputs.insert(word.to_string().to_lowercase().parse().unwrap(), *map.0);
+        }
+    }
+    outputs
 }
