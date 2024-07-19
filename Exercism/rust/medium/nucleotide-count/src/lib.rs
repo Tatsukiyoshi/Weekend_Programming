0f7af5b3897601ab7cd31e8158734baea8f72a30
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2024-07-19 14:15:48 +0900

    Add: Tested Nucleotide Count (#1676)

--- Exercism/rust/medium/nucleotide-count/src/lib.rs
+++ Exercism/rust/medium/nucleotide-count/src/lib.rs
@@ -1,9 +1,55 @@
 use std::collections::HashMap;
 
+pub const INITIAL_COUNT: usize = 0;
+
 pub fn count(nucleotide: char, dna: &str) -> Result<usize, char> {
-    todo!("How much of nucleotide type '{nucleotide}' is contained inside DNA string '{dna}'?");
+    println!("input: {}, {}", nucleotide, dna);
+    let result = nucleotide_counts(dna);
+
+    match result {
+        Ok(..) => {
+            let binding = result.unwrap();
+            if binding.len() == 0 {
+                Ok(0)
+            } else {
+                let count = binding.get(&nucleotide);
+                match count {
+                    Some(..) => Ok(*count.unwrap()),
+                    None => Err(nucleotide)
+                }
+            }
+        }
+        Err(e) => {
+            Err(e)
+        }
+    }
 }
 
 pub fn nucleotide_counts(dna: &str) -> Result<HashMap<char, usize>, char> {
-    todo!("How much of every nucleotide type is contained inside DNA string '{dna}'?");
+    let mut result: HashMap<char, usize> = HashMap::new();
+    let dna_chars = dna.chars();
+    let mut err_char = '*';
+
+    result.extend([('A', INITIAL_COUNT), ('C', INITIAL_COUNT), ('G', INITIAL_COUNT), ('T', INITIAL_COUNT)]);
+    for char in dna_chars {
+        println!("char = {}", char);
+        if char == 'A' || char == 'C' || char == 'G' || char == 'T' {
+            match result.get_key_value(&char) {
+                Some((key, value)) => {
+                    result.insert(*key, value + 1);
+                }
+                None => {
+                    result.insert(char, 1);
+                }
+            }
+            println!("{:?}", result);
+        } else {
+            err_char = char;
+        }
+    }
+    if err_char == '*' {
+        Ok(result)
+    } else {
+        Err(err_char)
+    }
 }
