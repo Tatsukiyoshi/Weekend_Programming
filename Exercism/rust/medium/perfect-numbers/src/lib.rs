8dd4bc0b9f821aa036ee7f53f99757c2d248f1d0
Shinya Watanabe <48684470+Tatsukiyoshi@users.noreply.github.com>
2024-07-23 10:55:18 +0900

    Add: Tested Perfect Numbers (#1676)

--- Exercism/rust/medium/perfect-numbers/src/lib.rs
+++ Exercism/rust/medium/perfect-numbers/src/lib.rs
@@ -6,5 +6,23 @@ pub enum Classification {
 }
 
 pub fn classify(num: u64) -> Option<Classification> {
-    todo!("classify {num}");
+    let mut aquliot_sum = 0;
+
+    if num == 0 {
+        return None;
+    }
+
+    for value in 1..num {
+        if num % value == 0 {
+            aquliot_sum = aquliot_sum + value;
+        }
+    }
+
+    if aquliot_sum == num {
+        Some(Classification::Perfect)
+    } else if aquliot_sum > num {
+        Some(Classification::Abundant)
+    } else {
+        Some(Classification::Deficient)
+    }
 }
