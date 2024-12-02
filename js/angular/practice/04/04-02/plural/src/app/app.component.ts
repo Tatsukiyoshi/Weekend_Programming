c6adfcebbb4924a20ececb0e0c04c65e160cdd49
Shinya Watanabe <taishow2007@live.jp>
2024-12-02 20:06:59 +0900

    Update: Projects of Chapter 04-02 to Angular 19.0.1 (#1810)

--- js/angular/practice/04/04-02/plural/src/app/app.component.ts
+++ js/angular/practice/04/04-02/plural/src/app/app.component.ts
@@ -1,6 +1,8 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
+  imports: [CommonModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
