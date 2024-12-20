3c65282e60c248263faf8fca9bcb701a985d4fbb
Shinya Watanabe <taishow2007@live.jp>
2024-12-21 00:51:06 +0900

    Update: Projects of Chapter 11 to Angular 19.0.5 (#1837)

--- js/angular/practice/11/bootstrap/src/main.ts
+++ js/angular/practice/11/bootstrap/src/main.ts
@@ -1,7 +1,18 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, TooltipModule.forRoot(), AccordionModule.forRoot()),
+        provideAnimations()
+    ]
+})
   .catch(err => console.error(err));
