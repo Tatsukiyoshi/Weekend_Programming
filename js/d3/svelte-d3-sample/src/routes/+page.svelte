8e6b34723754359c1b7ebaef9a912a256f1adf34
Shinya Watanabe <taishow2007@live.jp>
2025-03-29 15:36:30 +0900

    Add: Dynamic axes and change to curve (#2013)

--- js/d3/svelte-d3-sample/src/routes/+page.svelte
+++ js/d3/svelte-d3-sample/src/routes/+page.svelte
@@ -2,12 +2,16 @@
     import { onMount } from 'svelte';
     import LinePlot from './LinePlot.svelte';
     
-    let data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let data = [1, 3, 2, 5, 4, 7, 6, 10, 8, 9];
     
     onMount(() => {
         console.log('Component mounted');
         // You can add any additional logic here
     });
 </script>
+
+<h1>Welcome to your library project</h1>
+<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
 <LinePlot {data}></LinePlot>
--- js/d3/svelte-d3-sample/src/routes/LinePlot.svelte
+++ js/d3/svelte-d3-sample/src/routes/LinePlot.svelte
@@ -9,12 +9,18 @@
   export let marginBottom = 20;
   export let marginLeft = 20;
 
+  let gx: SVGGElement | null = null, gy: SVGGElement | null = null;
+
   $: x = d3.scaleLinear([0, data.length - 1], [marginLeft, width - marginRight]);
   $: extent = d3.extent(data) as Iterable<number>;
   $: y = d3.scaleLinear(extent, [height - marginBottom, marginTop]);
-  $: line = d3.line((d, i) => x(i), y);
+  $: line = d3.line((d, i) => x(i), y).curve(d3.curveNatural);
+  $: if (gy) d3.select(gy).call(d3.axisLeft(y));
+  $: if (gx) d3.select(gx).call(d3.axisBottom(x));
 </script>
 <svg width={width} height={height}>
+  <g bind:this={gx} transform="translate(0, {height - marginBottom})" />
+  <g bind:this={gy} transform="translate({marginLeft}, 0)" />
   <path fill="none" stroke="currentColor" stroke-width="1.5" d={line(data)} />
   <g fill="white" stroke="currentColor" stroke-width="1.5">
     {#each data as d, i}
