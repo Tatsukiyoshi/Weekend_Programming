8a681a8c7c69f98378dc2fd3feadce2602c651fe
Shinya Watanabe <taishow2007@live.jp>
2025-03-29 19:40:42 +0900

    Update: Svelte 5.25.3 (#2018)

--- js/svelte-react/svelte-test/src/routes/+page.svelte
+++ js/svelte-react/svelte-test/src/routes/+page.svelte
@@ -12,7 +12,7 @@
 	}
 </script>
 
-<h1>Welcome to SvelteKit 5.25.2</h1>
+<h1>Welcome to SvelteKit 5.25.3</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
 <main>
